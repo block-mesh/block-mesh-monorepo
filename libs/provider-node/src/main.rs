b1dc0e68e51a4b0e9d3b910590bf363f32e4306f
ohaddahan <ohaddahan@gmail.com>
2024-03-05 09:35:24 +0200

    yay

--- libs/provider-node/src/main.rs
+++ libs/provider-node/src/main.rs
@@ -1,143 +1,113 @@
-use async_trait::async_trait;
-use log::info;
-use prometheus::register_int_counter;
-use structopt::StructOpt;
+use axum::{
+    body::Body,
+    extract::Request,
+    http::{Method, StatusCode},
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 
-use pingora_core::server::configuration::Opt;
-use pingora_core::server::Server;
-use pingora_core::upstreams::peer::HttpPeer;
-use pingora_core::Result;
-use pingora_http::ResponseHeader;
-use pingora_proxy::{ProxyHttp, Session};
+use hyper::body::Incoming;
+use hyper::server::conn::http1;
+use hyper::upgrade::Upgraded;
+use std::net::SocketAddr;
+use tokio::net::{TcpListener, TcpStream};
+use tower::Service;
+use tower::ServiceExt;
 
-fn check_login(req: &pingora_http::RequestHeader) -> bool {
-    // implement you logic check logic here
-    req.headers.get("Authorization").map(|v| v.as_bytes()) == Some(b"password")
-}
+use hyper_util::rt::TokioIo;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-pub struct MyGateway {
-    req_metric: prometheus::IntCounter,
-}
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "http_proxy=trace,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-#[async_trait]
-impl ProxyHttp for MyGateway {
-    type CTX = ();
-    fn new_ctx(&self) -> Self::CTX {}
+    let router_svc = Router::new().route("/", get(|| async { "OK" }));
 
-    async fn upstream_peer(
-        &self,
-        session: &mut Session,
-        _ctx: &mut Self::CTX,
-    ) -> Result<Box<HttpPeer>> {
-        let addr = match session.req_header().get_header("Target") {
-            Some(target) => target,
-            None => {
-                return Err(pingora_core::Error::new("Target header is missing"));
+    let tower_service = tower::service_fn(move |req: Request<_>| {
+        let router_svc = router_svc.clone();
+        let req = req.map(Body::new);
+        async move {
+            if req.method() == Method::CONNECT {
+                proxy(req).await
+            } else {
+                router_svc.oneshot(req).await.map_err(|err| match err {})
             }
-        };
-
-        println!("target: {addr:?}");
-
-        let addr = if session.req_header().uri.path().starts_with("/family") {
-            ("1.0.0.1", 443)
-        } else {
-            ("1.1.1.1", 443)
-        };
-
-        info!("connecting to {addr:?}");
-
-        let peer = Box::new(HttpPeer::new(addr, true, "one.one.one.one".to_string()));
-        Ok(peer)
-    }
-
-    async fn request_filter(&self, session: &mut Session, _ctx: &mut Self::CTX) -> Result<bool> {
-        if session.req_header().uri.path().starts_with("/login")
-            && !check_login(session.req_header())
-        {
-            let _ = session.respond_error(403).await;
-            // true: early return as the response is already written
-            return Ok(true);
         }
-        Ok(false)
-    }
+    });
 
-    async fn response_filter(
-        &self,
-        _session: &mut Session,
-        upstream_response: &mut ResponseHeader,
-        _ctx: &mut Self::CTX,
-    ) -> Result<()>
-    where
-        Self::CTX: Send + Sync,
-    {
-        // replace existing header if any
-        upstream_response
-            .insert_header("Server", "MyGateway")
-            .unwrap();
-        // because we don't support h3
-        upstream_response.remove_header("alt-svc");
+    let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| {
+        tower_service.clone().call(request)
+    });
 
-        Ok(())
-    }
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    println!("listening on {}", addr);
 
-    async fn logging(
-        &self,
-        session: &mut Session,
-        _e: Option<&pingora_core::Error>,
-        ctx: &mut Self::CTX,
-    ) {
-        let response_code = session
-            .response_written()
-            .map_or(0, |resp| resp.status.as_u16());
-        info!(
-            "{} response code: {response_code}",
-            self.request_summary(session, ctx)
-        );
-
-        self.req_metric.inc();
+    let listener = TcpListener::bind(addr).await.unwrap();
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        let io = TokioIo::new(stream);
+        let hyper_service = hyper_service.clone();
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .preserve_header_case(true)
+                .title_case_headers(true)
+                .serve_connection(io, hyper_service)
+                .with_upgrades()
+                .await
+            {
+                println!("Failed to serve connection: {:?}", err);
+            }
+        });
     }
 }
 
-// RUST_LOG=INFO cargo run --example load_balancer
-// curl 127.0.0.1:6191 -H "Host: one.one.one.one"
-// curl 127.0.0.1:6190/family/ -H "Host: one.one.one.one"
-// curl 127.0.0.1:6191/login/ -H "Host: one.one.one.one" -I -H "Authorization: password"
-// curl 127.0.0.1:6191/login/ -H "Host: one.one.one.one" -I -H "Authorization: bad"
-// For metrics
-// curl 127.0.0.1:6192/
-fn main() {
-    env_logger::init();
+async fn proxy(req: Request) -> Result<Response, hyper::Error> {
+    tracing::trace!(?req);
+    println!("proxy reg {:?}", req);
 
-    println!(
-        r#"
-RUST_LOG=INFO cargo run --example load_balancer
-curl 127.0.0.1:6191 -H 'Host: one.one.one.one'
-curl 127.0.0.1:6190/family/ -H 'Host: one.one.one.one'
-curl 127.0.0.1:6191/login/ -H 'Host: one.one.one.one' -I -H 'Authorization: password'
-curl 127.0.0.1:6191/login/ -H 'Host: one.one.one.one' -I -H 'Authorization: bad'
-For metrics
-curl 127.0.0.1:6192/
-    "#
+    if let Some(host_addr) = req.uri().authority().map(|auth| auth.to_string()) {
+        tokio::task::spawn(async move {
+            match hyper::upgrade::on(req).await {
+                Ok(upgraded) => {
+                    if let Err(e) = tunnel(upgraded, host_addr).await {
+                        tracing::warn!("server io error: {}", e);
+                    };
+                }
+                Err(e) => tracing::warn!("upgrade error: {}", e),
+            }
+        });
+
+        Ok(Response::new(Body::empty()))
+    } else {
+        tracing::warn!("CONNECT host is not socket addr: {:?}", req.uri());
+        Ok((
+            StatusCode::BAD_REQUEST,
+            "CONNECT must be to a socket address",
+        )
+            .into_response())
+    }
+}
+
+async fn tunnel(upgraded: Upgraded, addr: String) -> std::io::Result<()> {
+    let mut server = TcpStream::connect(addr).await?;
+    let mut upgraded = TokioIo::new(upgraded);
+
+    let (from_client, from_server) =
+        tokio::io::copy_bidirectional(&mut upgraded, &mut server).await?;
+
+    tracing::debug!(
+        "client wrote {} bytes and received {} bytes",
+        from_client,
+        from_server
     );
 
-    // read command line arguments
-    let opt = Opt::from_args();
-    let mut my_server = Server::new(Some(opt)).unwrap();
-    my_server.bootstrap();
-
-    let mut my_proxy = pingora_proxy::http_proxy_service(
-        &my_server.configuration,
-        MyGateway {
-            req_metric: register_int_counter!("reg_counter", "Number of requests").unwrap(),
-        },
-    );
-    my_proxy.add_tcp("0.0.0.0:6191");
-    my_server.add_service(my_proxy);
-
-    let mut prometheus_service_http =
-        pingora_core::services::listening::Service::prometheus_http_service();
-    prometheus_service_http.add_tcp("127.0.0.1:6192");
-    my_server.add_service(prometheus_service_http);
-
-    my_server.run_forever();
+    Ok(())
 }
