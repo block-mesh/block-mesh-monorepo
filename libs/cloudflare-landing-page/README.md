1d79f0548d06ba2eb9d9e157902a765903d264b3
ohaddahan <ohaddahan@gmail.com>
2024-03-20 10:17:32 +0200

    cloudflare-landing-page

---
+++ libs/cloudflare-landing-page/README.md
@@ -0,0 +1,1 @@
+# Cloudflare Landing Page
---
+++ libs/cloudflare-landing-page/src/lib.rs
@@ -0,0 +1,36 @@
+use serde::{Deserialize, Serialize};
+use tracing_subscriber::fmt::format::Pretty;
+use tracing_subscriber::fmt::time::UtcTime;
+use tracing_subscriber::prelude::*;
+use tracing_web::{performance_layer, MakeConsoleWriter};
+use worker::*;
+
+#[event(start)]
+fn start() {
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .json()
+        .with_ansi(false) // Only partially supported across JavaScript runtimes
+        .with_timer(UtcTime::rfc_3339()) // std::time is not available in browsers
+        .with_writer(MakeConsoleWriter); // write events to the console
+    let perf_layer = performance_layer().with_details_from_fields(Pretty::default());
+    tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(perf_layer)
+        .init();
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RequestBody {
+    pub pubkey: String,
+    pub nonce: String,
+    pub signed_message: String,
+}
+
+#[event(fetch)]
+async fn main(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+    if req.method() != Method::Post {
+        return Response::error("Only POST Method allowed", 405);
+    }
+
+    Response::ok(format!("Hello, world! method = {:?}", req.method()))
+}
