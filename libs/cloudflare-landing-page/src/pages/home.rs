ca3f72c02a0d824bfbd29382f715a4d1f9af07e5
ohaddahan <ohaddahan@gmail.com>
2024-03-20 11:12:17 +0200

    remove

--- libs/cloudflare-landing-page/src/pages/home.rs
+++ libs/cloudflare-landing-page/src/pages/home.rs
@@ -80,7 +80,7 @@ pub static HOME_PAGE: &str = r###"
             </svg>
           </a>
         </div>
-        <p class="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">&copy; 2024 BlockMesh, Inc. All rights reserved.</p>
+        <p class="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">&copy; 2024 BlockMesh</p>
       </div>
     </div>
   </footer>
