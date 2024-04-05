0ecf4bae74e7cc6007cd40e21ec9bc75ce7f6539
ohaddahan <ohaddahan@gmail.com>
2024-04-05 22:19:26 +0300

    Progressing

---
+++ libs/client-node/src/modes/cli.rs
@@ -0,0 +1,64 @@
+use crate::cli_args::ClientNodeCliArgs;
+use crate::{cli_args, get_proxy};
+use block_mesh_solana_client::helpers::sign_message;
+use block_mesh_solana_client::manager::{FullRouteHeader, SolanaManager};
+use solana_client::client_error::reqwest;
+use std::net::IpAddr;
+use std::str::FromStr;
+use std::sync::Arc;
+use uuid::Uuid;
+
+pub async fn cli_mode(
+    solana_manager: Arc<SolanaManager>,
+    proxy_url: &str,
+    client_node_cli_args: ClientNodeCliArgs,
+) {
+    tracing::info!("Proxy URL: {}", proxy_url);
+
+    let nonce = Uuid::new_v4().to_string();
+    let signed_message = sign_message(&nonce, &solana_manager.get_keypair()).unwrap();
+    let solana_manager_header = FullRouteHeader::new(
+        nonce,
+        signed_message,
+        solana_manager.get_pubkey(),
+        solana_manager.get_api_token(),
+        "client-node".to_string(),
+    );
+    let proxy = get_proxy(proxy_url, &solana_manager_header).await.unwrap();
+    let local_address = IpAddr::from_str("0.0.0.0").unwrap();
+    let client = reqwest::Client::builder()
+        .local_address(local_address)
+        .proxy(proxy)
+        .build()
+        .unwrap();
+
+    let response: reqwest::Response = client
+        .get(&client_node_cli_args.target)
+        .send()
+        .await
+        .unwrap();
+    tracing::info!("RESPONSE HEADERS => {:?}", response.headers());
+    let content_type = match response.headers().get("content-type") {
+        None => cli_args::ResponseType::Text,
+        Some(content_type) => {
+            if content_type.to_str().unwrap().contains("application/json") {
+                cli_args::ResponseType::Json
+            } else {
+                cli_args::ResponseType::Text
+            }
+        }
+    };
+    match content_type {
+        cli_args::ResponseType::Json => {
+            let response: serde_json::Value = response.json().await.unwrap();
+            let pretty_json = serde_json::to_string_pretty(&response).unwrap();
+            tracing::info!("FINAL RESPONSE: {:?}", pretty_json);
+            println!("\n\n{}\n\n", pretty_json);
+        }
+        cli_args::ResponseType::Text => {
+            let response: String = response.text().await.unwrap();
+            tracing::info!("FINAL RESPONSE: {:?}", response);
+            println!("\n\n{}\n\n", response);
+        }
+    }
+}
