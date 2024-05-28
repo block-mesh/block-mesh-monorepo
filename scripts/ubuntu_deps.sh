83dcb3f8beb82ef083ab16980d6e8e97aa8fe177
ohaddahan <ohaddahan@gmail.com>
2024-05-28 09:27:30 +0300

    work in progress - deployment flow

---
+++ scripts/ubuntu_deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt install -y libfuse2 libharfbuzz0b libfontconfig libgbm-dev libthai0 libglu1-mesa-dev libegl1
+
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-vnc-on-ubuntu-20-04 :
+sudo apt install xfce4 xfce4-goodies tightvncserver -y
