4214aa61c030c3be151edada2b7064e479b6a907
ohaddahan <ohaddahan@gmail.com>
2024-06-02 19:19:50 +0300

    bump version + fix login/register redirects (#144)
    * bump version + fix login/register redirects
    
    * fml

--- libs/block-mesh-manager/src/routes/login/login_form.rs
+++ libs/block-mesh-manager/src/routes/login/login_form.rs
@@ -1,11 +1,21 @@
 use askama::Template;
 use askama_axum::IntoResponse;
+use axum::response::Redirect;
+use axum::Extension;
+use axum_login::AuthSession;
+
+use crate::middlewares::authentication::Backend;
 
 #[derive(Template)]
 #[template(path = "login.html")]
 struct LoginTemplate;
 
 #[tracing::instrument(name = "login")]
-pub async fn handler() -> impl IntoResponse {
-    LoginTemplate
+pub async fn handler(
+    Extension(auth): Extension<AuthSession<Backend>>,
+) -> Result<impl IntoResponse, Redirect> {
+    return match auth.user {
+        Some(_) => Err(Redirect::to("/dashboard")),
+        None => Ok(LoginTemplate),
+    };
 }
--- libs/block-mesh-manager/src/routes/register/register_form.rs
+++ libs/block-mesh-manager/src/routes/register/register_form.rs
@@ -1,11 +1,20 @@
+use crate::middlewares::authentication::Backend;
 use askama::Template;
 use askama_axum::IntoResponse;
+use axum::response::Redirect;
+use axum::Extension;
+use axum_login::AuthSession;
 
 #[derive(Template)]
 #[template(path = "register.html")]
 struct RegisterTemplate;
 
 #[tracing::instrument(name = "register")]
-pub async fn handler() -> impl IntoResponse {
-    RegisterTemplate
+pub async fn handler(
+    Extension(auth): Extension<AuthSession<Backend>>,
+) -> Result<impl IntoResponse, Redirect> {
+    return match auth.user {
+        Some(_) => Err(Redirect::to("/dashboard")),
+        None => Ok(RegisterTemplate),
+    };
 }
