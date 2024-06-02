537d71651d5bc44ac4d692782f1361d946471bc1
ohaddahan <ohaddahan@gmail.com>
2024-06-02 12:55:28 +0300

    nice

---
+++ libs/block-mesh-manager/src/database/invite_code/get_user_latest_invite_code.rs
@@ -0,0 +1,22 @@
+use crate::domain::invite_code::InviteCode;
+use sqlx::{Postgres, Transaction};
+use uuid::Uuid;
+
+#[tracing::instrument(name = "get_user_latest_invite_code", skip(transaction), ret, err)]
+pub(crate) async fn get_user_latest_invite_code(
+    transaction: &mut Transaction<'_, Postgres>,
+    user_id: Uuid,
+) -> anyhow::Result<InviteCode> {
+    Ok(sqlx::query_as!(
+        InviteCode,
+        r#"SELECT
+        id,
+        invite_code,
+        user_id,
+        created_at
+        FROM invite_codes WHERE user_id = $1 ORDER BY created_at DESC LIMIT 1"#,
+        user_id
+    )
+    .fetch_one(&mut **transaction)
+    .await?)
+}
--- libs/block-mesh-manager/src/routes/dashboard/get.rs
+++ libs/block-mesh-manager/src/routes/dashboard/get.rs
@@ -1,5 +1,7 @@
 use crate::database::daily_stat::get_daily_stats_by_user_id::get_daily_stats_by_user_id;
 use crate::database::daily_stat::get_users_tank::{get_users_rank, RankedUsers};
+use crate::database::invite_code::get_number_of_users_invited::get_number_of_users_invited;
+use crate::database::invite_code::get_user_latest_invite_code::get_user_latest_invite_code;
 use crate::domain::daily_stat::DailyStat;
 use crate::errors::error::Error;
 use crate::middlewares::authentication::Backend;
@@ -15,6 +17,8 @@ struct DashboardTemplate {
     pub overall_task_count: i64,
     pub rank: i64,
     pub daily_stats: Vec<DailyStat>,
+    pub invite_code: String,
+    pub number_of_users_invited: i64,
 }
 
 #[tracing::instrument(name = "dashboard", skip(auth))]
@@ -37,11 +41,19 @@ pub async fn handler(
             total_tasks_count: Some(0),
             rank: Some(0),
         });
+    let user_invite_code = get_user_latest_invite_code(&mut transaction, user.id)
+        .await
+        .map_err(Error::from)?;
+    let number_of_users_invited = get_number_of_users_invited(&mut transaction, user.id)
+        .await
+        .map_err(Error::from)?;
     transaction.commit().await.map_err(Error::from)?;
     let template = DashboardTemplate {
         daily_stats,
         overall_task_count,
         rank: my_rank.rank.unwrap_or_default(),
+        invite_code: user_invite_code.invite_code,
+        number_of_users_invited,
     };
     Ok(template)
 }
