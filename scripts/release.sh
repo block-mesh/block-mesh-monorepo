11a7a9792cfe757c76a7ef849b706364784ae6b4
ohaddahan <ohaddahan@gmail.com>
2024-06-02 16:17:36 +0300

    fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,6 +6,6 @@ heroku container:login
 docker pull blockmesh/block-mesh-manager:latest
 docker tag blockmesh/block-mesh-manager:latest registry.heroku.com/block-mesh-manager/web
 docker push registry.heroku.com/block-mesh-manager/web
-heroku container:push web -a block-mesh-manager
+#heroku container:push web -a block-mesh-manager
 heroku container:release web -a block-mesh-manager
 heroku restart -a block-mesh-manager
