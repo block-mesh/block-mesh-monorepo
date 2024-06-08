918e188bfec20d2faea62419303e4b02cf07568d
ohaddahan <ohaddahan@gmail.com>
2024-06-08 19:02:11 +0300

    going live

--- libs/extension/src/background/tasks.rs
+++ libs/extension/src/background/tasks.rs
@@ -85,7 +85,7 @@ pub async fn run_task(
     }
 }
 
-#[tracing::instrument(name = "submit_task", ret, err)]
+#[tracing::instrument(name = "submit_task", err)]
 pub async fn submit_task(
     base_url: &str,
     email: &str,
