db213a30211e326bedf5f8842d6a773b9d5ca8fb
ohaddahan <ohaddahan@gmail.com>
2024-06-18 10:54:58 +0300

    test tailwindcss (#192)
    * test tailwindcss
    
    * update workflow
    
    * fix-gz

---
+++ scripts/tailwind.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -x
+export _PWD="$(pwd)"
+export ROOT="$(git rev-parse --show-toplevel)"
+source "${ROOT}/scripts/setup.sh"
+cd "${ROOT}" || exit 1
+
+ensure npx tailwindcss -i ./input.css -o ./tailwind.css
+ensure wrangler r2 object put assets/tailwind.css --file tailwind.css
