fcaf63347e3a36c1d91ff83df08fc2c64ad68026
ohaddahan <ohaddahan@gmail.com>
2024-06-18 10:52:45 +0300

    fix-gz

--- scripts/tailwind.sh
+++ scripts/tailwind.sh
@@ -6,5 +6,4 @@ source "${ROOT}/scripts/setup.sh"
 cd "${ROOT}" || exit 1
 
 ensure npx tailwindcss -i ./input.css -o ./tailwind.css
-ensure gzip tailwind.css
-ensure wrangler r2 object put assets/tailwind.css.gz --file tailwind.css.gz
+ensure wrangler r2 object put assets/tailwind.css --file tailwind.css
