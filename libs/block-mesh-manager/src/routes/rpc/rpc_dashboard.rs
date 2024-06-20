aa903701a2fe52af37089b1489388fbcbf3ae3f8
ohaddahan <ohaddahan@gmail.com>
2024-06-20 12:37:43 +0300

    rpc dashboard filtering

--- libs/block-mesh-manager/src/routes/rpc/rpc_dashboard.rs
+++ libs/block-mesh-manager/src/routes/rpc/rpc_dashboard.rs
@@ -29,7 +29,9 @@ struct RpcDashboardTemplate {
 #[tracing::instrument(name = "rpc_dashboard")]
 pub async fn handler(Extension(pool): Extension<PgPool>) -> Result<impl IntoResponse, Error> {
     let mut transaction = pool.begin().await.map_err(Error::from)?;
-    let results = get_tasks_rpc_results(&mut transaction, 600_000).await?;
+    let results = get_tasks_rpc_results(&mut transaction, 600).await?;
+
+    tracing::info!("results: {:?}", results.len());
 
     let template = RpcDashboardTemplate {
         chrome_extension_link: BLOCK_MESH_CHROME_EXTENSION_LINK.to_string(),
