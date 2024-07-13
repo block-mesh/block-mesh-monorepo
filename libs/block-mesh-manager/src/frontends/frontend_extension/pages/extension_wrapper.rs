911c80e9a2d0a787642b37f2bbd75eef4677c0e7
ohaddahan <ohaddahan@gmail.com>
2024-07-13 10:47:32 +0300

    lint

--- libs/block-mesh-manager/src/frontends/frontend_extension/pages/extension_wrapper.rs
+++ libs/block-mesh-manager/src/frontends/frontend_extension/pages/extension_wrapper.rs
@@ -1,9 +1,11 @@
 use crate::frontends::frontend_extension::extension_state::ExtensionState;
+use leptos::logging::log;
 use leptos::*;
 
 #[component]
 pub fn ExtensionWrapper(children: ChildrenFn) -> impl IntoView {
     provide_context(ExtensionState::default());
+    log!("ExtensionWrapper");
     let state = use_context::<ExtensionState>().unwrap();
     let resource = ExtensionState::init_resource(state);
     let (view, _) = create_signal(move || children().into_view());
