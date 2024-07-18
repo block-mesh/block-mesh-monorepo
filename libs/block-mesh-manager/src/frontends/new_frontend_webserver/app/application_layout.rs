eeaf3e9731b02fc8ad1741fa5c697247e0beb7b8
Maccesch <maccesch@synphonyte.com>
2024-07-18 22:29:55 +0100

    porting new ui components

--- libs/block-mesh-manager/src/frontends/new_frontend_webserver/app/application_layout.rs
+++ libs/block-mesh-manager/src/frontends/new_frontend_webserver/app/application_layout.rs
@@ -1,3 +1,7 @@
+use crate::frontends::new_frontend_webserver::components::{
+    Navbar, NavbarSection, NavbarSpacer, Sidebar, SidebarBody, SidebarHeader, SidebarItem,
+    SidebarLabel, SidebarLayout, SidebarSection,
+};
 use leptos::*;
 use leptos_router::use_location;
 use tailwind_fuse::*;
@@ -25,10 +29,84 @@ pub fn OnlineChip(#[prop(into)] is_online: MaybeSignal<bool>) -> impl IntoView {
 }
 
 #[component]
+pub fn ApplicationNavbar() -> impl IntoView {
+    view! {
+        <Navbar>
+            <NavbarSpacer/>
+
+            <NavbarSection>
+                <OnlineChip is_online=true/>
+            </NavbarSection>
+        </Navbar>
+    }
+}
+
+#[component]
+pub fn ApplicationSidebar() -> impl IntoView {
+    view! {
+        <Sidebar>
+            <SidebarHeader>
+                <Dropdown>
+                    <DropdownButton as={SidebarItem}>
+                        <Avatar src="/app/logo.png"/>
+                        <SidebarLabel>BlockMesh</SidebarLabel>
+                        <OnlineChip class="desktop-online-chip" is_online=true></OnlineChip>
+                    </DropdownButton>
+                </Dropdown>
+            </SidebarHeader>
+
+            <SidebarBody>
+                <SidebarSection>
+                    <SidebarItem href="/">
+                        <HomeIcon/>
+                        <SidebarLabel>Dashboard</SidebarLabel>
+                    </SidebarItem>
+                    <SidebarItem href="/referer">
+                        <LinkIcon/>
+                        <SidebarLabel>Referrals</SidebarLabel>
+                    </SidebarItem>
+                    <SidebarItem href="/extension">
+                        <LinkIcon/>
+                        <SidebarLabel>Extension</SidebarLabel>
+                    </SidebarItem>
+                </SidebarSection>
+
+                <SidebarSpacer/>
+
+                <SidebarSection>
+                    <SidebarItem href="logout">
+                        <ArrowRightStartOnRectangleIcon/>
+                        <SidebarLabel>Logout</SidebarLabel>
+                    </SidebarItem>
+                </SidebarSection>
+            </SidebarBody>
+
+            <SidebarFooter class="max-lg:hidden">
+                <Dropdown>
+                    <DropdownButton as={SidebarItem}>
+        <span class="flex min-w-0 items-center gap-3">
+          <span class="min-w-0">
+            <span class="block truncate text-sm/5 font-medium text-zinc-950 text-orange">Ohad</span>
+            <span class="block truncate text-xs/5 font-normal text-zinc-500 dark:text-zinc-400">
+              Ohad@blockmesh.com
+            </span>
+          </span>
+        </span>
+                    </DropdownButton>
+                </Dropdown>
+            </SidebarFooter>
+        </Sidebar>
+    }
+}
+
+#[component]
 pub fn ApplicationLayout(children: Children) -> impl IntoView {
     let pathname = use_location().pathname;
 
     view! {
-
+        <SidebarLayout
+            navbar=ApplicationNavbar
+            sidebar=ApplicationSidebar
+        >
     }
 }
--- libs/block-mesh-manager/src/frontends/new_frontend_webserver/components/mod.rs
+++ libs/block-mesh-manager/src/frontends/new_frontend_webserver/components/mod.rs
@@ -2,10 +2,12 @@ mod alert;
 mod avatar;
 mod if_let;
 mod navbar;
+mod sidebar;
 mod sidebar_layout;
 
 pub use alert::*;
 pub use avatar::*;
 pub use if_let::*;
 pub use navbar::*;
+pub use sidebar::*;
 pub use sidebar_layout::*;
