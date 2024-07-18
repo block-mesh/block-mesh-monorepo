1a41e0f13802dd1457b72c2e798c183fdb2e03c6
Maccesch <maccesch@synphonyte.com>
2024-07-18 22:29:55 +0100

    porting new ui components

--- libs/block-mesh-manager/src/frontends/mod.rs
+++ libs/block-mesh-manager/src/frontends/mod.rs
@@ -4,4 +4,5 @@ pub mod context;
 pub mod frontend_extension;
 pub mod frontend_tauri;
 pub mod frontend_webserver;
+pub mod new_frontend_webserver;
 pub mod wrapper;
---
+++ libs/block-mesh-manager/src/frontends/new_frontend_webserver/components/sidebar.rs
@@ -0,0 +1,155 @@
+use leptos::html::A;
+use leptos::*;
+use leptos_router::{use_location, use_navigate};
+use tailwind_fuse::*;
+
+#[component]
+pub fn Sidebar(
+    #[prop(into, optional)] class: MaybeSignal<String>,
+    children: Children,
+) -> impl IntoView {
+    let class = move || tw_merge!(class.get(), "flex h-full min-h-0 flex-col");
+
+    view! {
+        <nav class=class>
+            {children()}
+        </nav>
+    }
+}
+
+#[component]
+pub fn SidebarHeader(
+    #[prop(into, optional)] class: MaybeSignal<String>,
+    children: Children,
+) -> impl IntoView {
+    let class = move || {
+        tw_merge!(class.get(), "flex flex-col border-b border-zinc-950/5 p-4 dark:border-white/5 [&>[data-slot=section]+[data-slot=section]]:mt-2.5")
+    };
+
+    view! {
+        <div class=class>
+            {children()}
+        </div>
+    }
+}
+
+#[component]
+pub fn SidebarBody(
+    #[prop(into, optional)] class: MaybeSignal<String>,
+    children: Children,
+) -> impl IntoView {
+    let class = move || {
+        tw_merge!(class.get(), "flex flex-1 flex-col overflow-y-auto p-4 [&>[data-slot=section]+[data-slot=section]]:mt-8")
+    };
+
+    view! {
+        <div class=class>
+            {children()}
+        </div>
+    }
+}
+
+#[component]
+pub fn SidebarFooter(
+    #[prop(into, optional)] class: MaybeSignal<String>,
+    children: Children,
+) -> impl IntoView {
+    let class = move || {
+        tw_merge!(class.get(), "flex flex-col border-t border-zinc-950/5 p-4 dark:border-white/5 [&>[data-slot=section]+[data-slot=section]]:mt-2.5")
+    };
+
+    view! {
+        <div class=class>
+            {children()}
+        </div>
+    }
+}
+
+#[component]
+pub fn SidebarSection(
+    #[prop(into, optional)] class: MaybeSignal<String>,
+    children: Children,
+) -> impl IntoView {
+    let class = move || tw_merge!(class.get(), "flex flex-col gap-0.5");
+
+    view! {
+        <div class=class data-slot="section">
+            {children()}
+        </div>
+    }
+}
+
+#[component]
+pub fn SidebarSpacer(#[prop(into, optional)] class: MaybeSignal<String>) -> impl IntoView {
+    let class = move || tw_merge!(class.get(), "mt-8 flex-1");
+
+    view! {
+        <div class=class aria-hidden="true">
+        </div>
+    }
+}
+
+#[component]
+pub fn SidebarItem(
+    #[prop(into, optional)] class: MaybeSignal<String>,
+    #[prop(into)] href: String,
+    link_ref: NodeRef<A>,
+    children: Children,
+) -> impl IntoView {
+    let pathname = use_location().pathname;
+    let current = Signal::derive(move || pathname.get().starts_with(&href));
+
+    let span_class = move || tw_merge!(class.get(), "relative");
+
+    let class = move || {
+        tw_merge!(
+            current.get().then_some("text-darkOrange"),
+            // Base
+            "flex w-full items-center gap-3 rounded-lg px-2 py-2.5 text-left text-base/6 font-medium text-zinc-950 sm:py-2 sm:text-sm/5",
+            // Leading icon/icon-only
+            "data-[slot=icon]:*:size-6 data-[slot=icon]:*:shrink-0 data-[slot=icon]:*:fill-zinc-500 sm:data-[slot=icon]:*:size-5",
+            // Trailing icon (down chevron or similar)
+            "data-[slot=icon]:last:*:ml-auto data-[slot=icon]:last:*:size-5 sm:data-[slot=icon]:last:*:size-6",
+            // Avatar
+            "data-[slot=avatar]:*:-m-0.5 data-[slot=avatar]:*:size-7 data-[slot=avatar]:*:[--ring-opacity:10%] sm:data-[slot=avatar]:*:size-6",
+            // Hover
+            "data-[hover]:bg-zinc-950/5 data-[slot=icon]:*:data-[hover]:fill-zinc-950",
+            // Active
+            "data-[active]:bg-zinc-950/5 data-[slot=icon]:*:data-[active]:fill-zinc-950",
+            // Current
+            "data-[slot=icon]:*:data-[current]:fill-zinc-950",
+            // Dark mode
+            "dark:text-white dark:data-[slot=icon]:*:fill-zinc-400",
+            "dark:data-[hover]:bg-white/5 dark:data-[slot=icon]:*:data-[hover]:fill-white",
+            "dark:data-[active]:bg-white/5 dark:data-[slot=icon]:*:data-[active]:fill-white",
+            "dark:data-[slot=icon]:*:data-[current]:fill-white",
+        )
+    };
+
+    view! {
+        <span class=span_class>
+            <Show when=move || current.get()>
+                // Current indicator
+                <span class="absolute inset-y-2 -left-4 w-0.5 rounded-full bg-zinc-950 dark:bg-white"></span>
+            </Show>
+        </span>
+
+        <a href=href node_ref=link_ref class=class data-current=current>
+            {children()}
+        </a>
+    }
+}
+
+#[component]
+pub fn SidebarLabel(
+    #[prop(into, optional)] class: MaybeSignal<String>,
+    children: Children,
+) -> impl IntoView {
+    let class = move || tw_merge!(class.get(), "truncate");
+
+    view! {
+        <span class=class data-slot="section">
+            {children()}
+        </span>
+    }
+}
