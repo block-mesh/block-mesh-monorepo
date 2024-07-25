03062943f229ab4f928205593559b5eb4c29f72c
ohaddahan <ohaddahan@gmail.com>
2024-07-25 18:46:08 +0300

    fml

--- libs/block-mesh-manager/src/frontends/frontend_webserver/components/metrics.rs
+++ libs/block-mesh-manager/src/frontends/frontend_webserver/components/metrics.rs
@@ -7,8 +7,9 @@ use leptos_router::A;
 #[component]
 pub fn MetricsComponent() -> impl IntoView {
     let async_data = WebAppContext::get_dashboard_data();
+    let status_color = Signal::derive(move || "green-300");
     view! {
-        <div class="m-2">
+        <div class="m-2 grid grid-cols-1 md:grid-cols-2 gap-4">
             <div class="border-off-white border m-2 relative overflow-hidden rounded-[30px] pt-6 md:pt-[33px] pb-7 md:pb-[39px] pl-[11px] md:pl-[44px]">
                 <img
                     alt="back"
@@ -24,6 +25,7 @@ pub fn MetricsComponent() -> impl IntoView {
                 <div class="relative z-[1]">
                     <div class="flex items-center text-[#808080] leading-[153%] max-md:text-xs max-md:ml-1.5 text-cyan">
                         <svg
+                            class="mr-2"
                             fill="none"
                             viewBox="0 0 24 24"
                             stroke-width="1.5"
@@ -62,6 +64,57 @@ pub fn MetricsComponent() -> impl IntoView {
                             <ChromeIcon/>
                         </A>
                     </p>
+                </div>
+            </div>
+
+            <div class="border-off-white border m-2 relative overflow-hidden rounded-[30px] pt-6 md:pt-[33px] pb-7 md:pb-[39px] pl-[11px] md:pl-[44px]">
+                <img
+                    alt="back"
+                    loading="lazy"
+                    width="808"
+                    height="433"
+                    decoding="async"
+                    data-nimg="1"
+                    class="absolute top-0 right-0 h-full w-[70%] max-w-[800px] object-cover object-left"
+                    src="https://imagedelivery.net/3RKw_J_fJQ_4KpJP3_YgXA/b589b8b1-ae33-488c-e7d6-0259a1cd0d00/public"
+                    style="color: transparent;"
+                />
+                <div class="relative z-[1]">
+                    <div class=move || {
+                        format!(
+                            "flex items-center text-[#808080] leading-[153%] max-md:text-xs max-md:ml-1.5 text-{}",
+                            status_color.get(),
+                        )
+                    }>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="mr-2"
+                            width="16"
+                            height="16"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M8.288 15.038a5.25 5.25 0 0 1 7.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 0 1 1.06 0Z"
+                            ></path>
+                        </svg>
+                        Network Status
+                    </div>
+                    <div class="text-cyan mt-[7px] md:mt-2.5 text-[30px] md:text-[40px] font-semibold leading-[116%] max-md:ml-1.5">
+                        {move || {
+                            let p = match async_data.get() {
+                                Some(Some(response)) => response.points,
+                                _ => 0f64,
+                            };
+                            format!("{:.3}", p)
+                        }}
+
+                    </div>
+                    <p class="text-sm font-medium leading-6 text-off-white">Current Uptime</p>
                 </div>
             </div>
         </div>
