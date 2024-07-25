4449e3deba4f2b9f50b3a07b7064228b08b0f417
ohaddahan <ohaddahan@gmail.com>
2024-07-25 16:53:56 +0300

    Indexes and move delete to background

---
+++ libs/block-mesh-manager/migrations/20240725133539_add-uptime-user_id-created_at-index.sql
@@ -0,0 +1,2 @@
+CREATE INDEX uptime_reports_user_id_created_at ON uptime_reports (user_id, created_at);
+CREATE INDEX bandwidth_reports_user_id_created_at ON bandwidth_reports (user_id, created_at);
