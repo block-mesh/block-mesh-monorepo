b0a7cc9e2f393543dda11d219a9cc373f2210f36
Maccesch <maccesch@synphonyte.com>
2024-07-27 17:20:48 +0100

    referral cosmetics

--- libs/block-mesh-manager/src/frontends/new_frontend_webserver/app/referer.rs
+++ libs/block-mesh-manager/src/frontends/new_frontend_webserver/app/referer.rs
@@ -13,7 +13,7 @@ pub fn RefererRank(
     #[prop(into)] step: u8,
     #[prop(into, optional)] is_complete: bool,
 ) -> impl IntoView {
-    let class = tw_merge!(
+    let class = tw_join!(
         "absolute left-0 top-0 w-1 lg:bottom-0 lg:top-auto lg:w-full",
         is_complete.then_some("bg-blue shadow-light h-px")
     );
@@ -189,7 +189,7 @@ pub fn Orders() -> impl IntoView {
         <ApplicationLayout>
             <div class="flex items-end justify-between gap-4">
                 <Heading>Referrals</Heading>
-                <button class="-my-0.5 cursor-pointer">
+                <button class="-my-0.5 cursor-pointer relative isolate inline-flex items-center justify-center gap-x-2 rounded-lg border text-base/6 font-semibold px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] sm:px-[calc(theme(spacing.3)-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] sm:text-sm/6 focus:outline-none data-[focus]:outline data-[focus]:outline-2 data-[focus]:outline-offset-2 data-[focus]:outline-blue-500 data-[disabled]:opacity-50 [&>[data-slot=icon]]:-mx-0.5 [&>[data-slot=icon]]:my-0.5 [&>[data-slot=icon]]:size-5 [&>[data-slot=icon]]:shrink-0 [&>[data-slot=icon]]:text-[--btn-icon] [&>[data-slot=icon]]:sm:my-1 [&>[data-slot=icon]]:sm:size-4 forced-colors:[--btn-icon:ButtonText] forced-colors:data-[hover]:[--btn-icon:ButtonText] border-transparent bg-[--btn-border] dark:bg-[--btn-bg] before:absolute before:inset-0 before:-z-10 before:rounded-[calc(theme(borderRadius.lg)-1px)] before:bg-[--btn-bg] before:shadow dark:before:hidden dark:border-white/5 after:absolute after:inset-0 after:-z-10 after:rounded-[calc(theme(borderRadius.lg)-1px)] after:shadow-[shadow:inset_0_1px_theme(colors.white/15%)] after:data-[active]:bg-[--btn-hover-overlay] after:data-[hover]:bg-[--btn-hover-overlay] dark:after:-inset-px dark:after:rounded-lg before:data-[disabled]:shadow-none after:data-[disabled]:shadow-none text-white [--btn-bg:theme(colors.zinc.900)] [--btn-border:theme(colors.zinc.950/90%)] [--btn-hover-overlay:theme(colors.white/10%)] dark:text-white dark:[--btn-bg:theme(colors.zinc.600)] dark:[--btn-hover-overlay:theme(colors.white/5%)] [--btn-icon:theme(colors.zinc.400)] data-[active]:[--btn-icon:theme(colors.zinc.300)] data-[hover]:[--btn-icon:theme(colors.zinc.300)] cursor-default">
                     <span class="material-symbols-outlined">link</span>
                     Copy Referer Link
                 </button>
