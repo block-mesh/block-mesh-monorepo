7ad891dac3784bd662a421a9b641016ffcff89f1
Marc-Stefan Cassola <maccesch@synphonyte.com>
2024-07-30 19:58:37 +0100

    fixed mobile view for new ui (#316)
    * fixed mobile view for new ui
    
    * sidebar fix
    
    ---------
    
    Co-authored-by: ohaddahan <ohaddahan@gmail.com>

--- libs/block-mesh-manager/src/frontends/new_frontend_webserver/components/sidebar_layout.rs
+++ libs/block-mesh-manager/src/frontends/new_frontend_webserver/components/sidebar_layout.rs
@@ -5,7 +5,7 @@ use tailwind_fuse::*;
 #[component]
 pub fn OpenMenuIcon() -> impl IntoView {
     view! {
-        <svg viewBox="0 0 20 20" aria-hidden="true">
+        <svg data-slot="icon" viewBox="0 0 20 20" aria-hidden="true">
             <path d="M2 6.75C2 6.33579 2.33579 6 2.75 6H17.25C17.6642 6 18 6.33579 18 6.75C18 7.16421 17.6642 7.5 17.25 7.5H2.75C2.33579 7.5 2 7.16421 2 6.75ZM2 13.25C2 12.8358 2.33579 12.5 2.75 12.5H17.25C17.6642 12.5 18 12.8358 18 13.25C18 13.6642 17.6642 14 17.25 14H2.75C2.33579 14 2 13.6642 2 13.25Z"></path>
         </svg>
     }
@@ -14,7 +14,7 @@ pub fn OpenMenuIcon() -> impl IntoView {
 #[component]
 pub fn CloseMenuIcon() -> impl IntoView {
     view! {
-        <svg viewBox="0 0 20 20" aria-hidden="true">
+        <svg data-slot="icon" viewBox="0 0 20 20" aria-hidden="true">
             <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z"></path>
         </svg>
     }
@@ -30,10 +30,11 @@ where
     CloseFn: Fn() + Clone + 'static,
 {
     let aside_class =
-        Signal::derive(move || tw_join!("lg:hidden z-20", open.get().then_some("hidden")));
+        Signal::derive(move || tw_join!("lg:hidden", (!open.get()).then_some("hidden")));
+
     let backdrop_class = move || {
         tw_join!(
-            "fixed inset-0 bg-black/30 z-10",
+            "fixed inset-0 bg-black/30",
             "w-screen h-screen",
             "transition data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in",
             aside_class.get()
@@ -42,28 +43,28 @@ where
 
     view! {
         <aside class=aside_class role="dialog" aria-modal="true">
-            <div class="fixed inset-y-0 w-full max-w-80 p-2 transition duration-300 ease-in-out data-[closed]:-translate-x-full"></div>
-        </aside>
-
-        <div
-            class=backdrop_class
-            aria-hidden="true"
-            on:click={
-                let on_close = on_close.clone();
-                move |_| on_close()
-            }
-        >
-
-            <div class="flex h-full flex-col rounded-lg bg-white shadow-sm ring-1 ring-zinc-950/5 dark:bg-zinc-900 dark:ring-white/10">
-                <div class="-mb-3 px-4 pt-3">
-                    <NavbarItem on_click=on_close aria_label="Close navigation">
-                        <CloseMenuIcon/>
-                    </NavbarItem>
-                </div>
-
-                {children()}
+            <div
+                class=backdrop_class
+                aria-hidden="true"
+                on:click={
+                    let on_close = on_close.clone();
+                    move |_| on_close()
+                }
+            >
             </div>
-        </div>
+            <div class="fixed inset-y-0 w-full max-w-80 p-2 transition duration-300 ease-in-out data-[closed]:-translate-x-full z-10">
+
+                <div class="flex h-full flex-col rounded-lg bg-white shadow-sm ring-1 ring-zinc-950/5 dark:bg-zinc-900 dark:ring-white/10">
+                    <div class="-mb-3 px-4 pt-3">
+                        <NavbarItem on_click=on_close aria_label="Close navigation">
+                            <CloseMenuIcon/>
+                        </NavbarItem>
+                    </div>
+
+                    {children()}
+                </div>
+            </div>
+        </aside>
     }
 }
 
