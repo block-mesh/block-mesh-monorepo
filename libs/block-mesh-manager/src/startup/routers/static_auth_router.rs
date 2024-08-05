34157625ea2ef7d57a24d3d090ebcc6f76c6d056
ohaddahan <ohaddahan@gmail.com>
2024-08-05 17:38:51 +0300

    add missing route

--- libs/block-mesh-manager/src/startup/routers/static_auth_router.rs
+++ libs/block-mesh-manager/src/startup/routers/static_auth_router.rs
@@ -7,9 +7,14 @@ use axum::Router;
 use block_mesh_common::routes_enum::RoutesEnum;
 
 pub fn get_static_auth_router() -> Router<Arc<AppState>> {
-    let auth_router = Router::new().route(
-        RoutesEnum::Static_Auth_Logout.to_string().as_str(),
-        get(routes::logout::get::handler),
-    );
+    let auth_router = Router::new()
+        .route(
+            RoutesEnum::Static_Auth_Logout.to_string().as_str(),
+            get(routes::logout::get::handler),
+        )
+        .route(
+            RoutesEnum::Static_Auth_Dashboard.to_string().as_str(),
+            get(routes::dashboard::get::handler).post(routes::dashboard::post::handler),
+        );
     auth_router
 }
