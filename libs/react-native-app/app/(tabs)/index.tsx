75e67b9cbe5bb18fd43bcbe49cedec22b07fef65
ohaddahan <ohaddahan@gmail.com>
2024-08-12 12:17:50 +0300

    manifest changes

--- libs/react-native-app/app/(tabs)/index.tsx
+++ libs/react-native-app/app/(tabs)/index.tsx
@@ -70,14 +70,9 @@ export default function HomeScreen() {
 
   async function stop() {
     console.log('start stop 0')
-    const r = await fetch('https://distinct-bison-merely.ngrok-free.app/health_check?hello=https').then(() => {
+    const r = await fetch('https://distinct-bison-merely.ngrok-free.app/health_check?hello=https&from=ui').then(() => {
       console.log('success')
     }, (e) => console.log('error', e))
-    console.log('start stop 1', r)
-    const z = await fetch('http://localhost:8000/health_check?hello=http').then(() => {
-      console.log('success')
-    }, (e) => console.log('error', e))
-    console.log('start stop 2', z)
     await BackgroundService.stop()
     console.log('hello', MyRustModule.hello())
     const x = await MyRustModule.stop_lib()
