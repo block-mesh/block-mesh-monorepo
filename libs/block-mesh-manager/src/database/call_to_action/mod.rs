a4eb60cc62a6fd5a36ce37f1b7aea45821bcdcc9
ohaddahan <ohaddahan@gmail.com>
2024-08-12 23:17:23 +0300

    sheesh

---
+++ libs/block-mesh-manager/src/database/call_to_action/mod.rs
@@ -0,0 +1,2 @@
+pub mod get_or_create_call_to_action;
+pub mod get_user_calls_to_action;
--- libs/block-mesh-manager/src/startup/routers/static_auth_router.rs
+++ libs/block-mesh-manager/src/startup/routers/static_auth_router.rs
@@ -17,6 +17,14 @@ pub fn get_static_auth_router() -> Router<Arc<AppState>> {
             get(routes::twitter::login::login),
         )
         .route(
+            RoutesEnum::Static_Auth_Edit_Invite.to_string().as_str(),
+            post(routes::invite_codes::edit_invite_code_post::handler),
+        )
+        .route(
+            RoutesEnum::Static_Auth_Call_To_Action.to_string().as_str(),
+            post(routes::call_to_action::post::handler),
+        )
+        .route(
             RoutesEnum::Static_Auth_Dashboard.to_string().as_str(),
             post(routes::dashboard::post::handler),
         );
