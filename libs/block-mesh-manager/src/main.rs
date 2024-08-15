6d8d790cf510ce9241786eaf189b741d40ef6c68
ohaddahan <ohaddahan@gmail.com>
2024-08-15 18:50:29 +0300

    prep server side

--- libs/block-mesh-manager/src/main.rs
+++ libs/block-mesh-manager/src/main.rs
@@ -2,12 +2,12 @@
 #![deny(elided_lifetimes_in_paths)]
 #![deny(unreachable_pub)]
 
-use block_mesh_common::interfaces::ws_api::WsMessage;
-use block_mesh_manager::worker::ws_worker::{ws_worker_rx, ws_worker_tx};
 use cfg_if::cfg_if;
-use tokio::sync::broadcast;
 
 cfg_if! { if #[cfg(feature = "ssr")] {
+    use block_mesh_common::interfaces::ws_api::WsMessage;
+    use block_mesh_manager::worker::ws_worker::{ws_worker_rx, ws_worker_tx};
+    use tokio::sync::broadcast;
     use std::env;
     use block_mesh_manager::worker::db_agg::{db_agg, UpdateBulkMessage};
     use logger_general::tracing::setup_tracing_stdout_only;
