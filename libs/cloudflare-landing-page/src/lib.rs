6b36d69234547cba2b45af5d676d630770f60939
ohaddahan <ohaddahan@gmail.com>
2024-08-17 13:39:42 +0300

    tmp

--- libs/cloudflare-landing-page/src/lib.rs
+++ libs/cloudflare-landing-page/src/lib.rs
@@ -26,32 +26,10 @@ fn start() {
 
 #[derive(Template)]
 #[template(path = "home.html")]
-struct Home {
-    pub chrome_extension_link: String,
-    pub app_server: String,
-    pub github: String,
-    pub twitter: String,
-    pub gitbook: String,
-    pub logo: String,
-    // pub image: String,
-    pub support: String,
-    pub chat: String,
-}
+struct Home {}
 
 #[event(fetch)]
 async fn main(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
-    let response = Home {
-        chrome_extension_link: BLOCK_MESH_CHROME_EXTENSION_LINK.to_string(),
-        app_server: BLOCK_MESH_APP_SERVER.to_string(),
-        github: BLOCK_MESH_GITHUB.to_string(),
-        twitter: BLOCK_MESH_TWITTER.to_string(),
-        gitbook: BLOCK_MESH_GITBOOK.to_string(),
-        logo: BLOCK_MESH_LOGO.to_string(),
-        // image: BLOCK_MESH_LANDING_PAGE_IMAGE.to_string(),
-        support: BLOCK_MESH_SUPPORT_EMAIL.to_string(),
-        chat: BLOCK_MESH_SUPPORT_CHAT.to_string(),
-    }
-    .render()
-    .unwrap();
+    let response = Home {}.render().unwrap();
     Response::from_html(response)
 }
