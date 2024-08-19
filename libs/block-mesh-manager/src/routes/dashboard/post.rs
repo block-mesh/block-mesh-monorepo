c8839b07dbcf570c7d658ed2472c260f2652e6c3
ohaddahan <ohaddahan@gmail.com>
2024-08-19 17:07:18 +0300

    fix disconnect

--- libs/block-mesh-manager/src/routes/dashboard/post.rs
+++ libs/block-mesh-manager/src/routes/dashboard/post.rs
@@ -1,6 +1,8 @@
+use axum::extract::State;
 use axum::{Extension, Json};
 use axum_login::AuthSession;
 use sqlx::PgPool;
+use std::sync::Arc;
 #[allow(unused_imports)]
 use tracing::Level;
 
@@ -9,16 +11,18 @@ use block_mesh_common::interfaces::server_api::DashboardResponse;
 use crate::errors::error::Error;
 use crate::middlewares::authentication::Backend;
 use crate::routes::dashboard::dashboard_data_extractor::dashboard_data_extractor;
+use crate::startup::application::AppState;
 
-#[tracing::instrument(name = "dashboard post", skip(auth), level = "trace",  err(level = Level::TRACE))]
+#[tracing::instrument(name = "dashboard post", skip(auth, state), level = "trace",  err(level = Level::TRACE))]
 pub async fn handler(
     Extension(pool): Extension<PgPool>,
+    State(state): State<Arc<AppState>>,
     Extension(auth): Extension<AuthSession<Backend>>,
 ) -> Result<Json<DashboardResponse>, Error> {
     let transaction = pool.begin().await.map_err(Error::from)?;
     let user = auth.user.ok_or(Error::UserNotFound)?;
     let user_id = user.id;
     transaction.commit().await.map_err(Error::from)?;
-    let data = dashboard_data_extractor(&pool, user_id).await?;
+    let data = dashboard_data_extractor(&pool, user_id, state).await?;
     Ok(Json(data))
 }
