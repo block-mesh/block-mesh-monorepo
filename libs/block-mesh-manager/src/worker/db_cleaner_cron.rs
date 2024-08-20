4ff183735b0fd4b5e189fb2fce470aa769c6b8a6
ohaddahan <ohaddahan@gmail.com>
2024-08-20 11:13:36 +0300

    remove uptime shit (#427)

--- libs/block-mesh-manager/src/worker/db_cleaner_cron.rs
+++ libs/block-mesh-manager/src/worker/db_cleaner_cron.rs
@@ -4,11 +4,10 @@ use crate::database::ip_address::enrich_ip_address::enrich_ip_address;
 use crate::database::ip_address::get_ip_address::get_ip_address;
 use crate::database::ip_address::get_opt_ip_address::get_opt_ip_address;
 use crate::database::uptime_report::delete_uptime_report_by_time_for_all::delete_uptime_report_by_time_for_all;
-use crate::database::uptime_report::enrich_uptime_report::enrich_uptime_report;
 use crate::database::users_ip::get_or_create_users_ip::get_or_create_users_ip;
 use crate::errors::error::Error;
 use block_mesh_common::constants::BLOCK_MESH_IP_WORKER;
-use block_mesh_common::interfaces::ip_data::{IPData, IpDataPostRequest, LocatorDe};
+use block_mesh_common::interfaces::ip_data::{IPData, IpDataPostRequest};
 use reqwest::{Client, ClientBuilder};
 use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
@@ -19,7 +18,6 @@ use uuid::Uuid;
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct EnrichIp {
     pub user_id: Uuid,
-    pub uptime_id: Uuid,
     pub ip: String,
 }
 
@@ -31,10 +29,7 @@ pub async fn db_cleaner_cron(
         .timeout(Duration::from_secs(3))
         .build()
         .unwrap_or_default();
-    let thread_pool = rayon::ThreadPoolBuilder::new()
-        .num_threads(16)
-        .build()
-        .unwrap();
+    let thread_pool = rayon::ThreadPoolBuilder::new().num_threads(16).build()?;
 
     while let Some(job) = rx.recv().await {
         let pool = pool.clone();
@@ -78,26 +73,7 @@ pub async fn enrich_ip_and_cleanup(
     get_or_create_users_ip(&mut transaction, &job.user_id, &ip_address.id)
         .await
         .map_err(Error::from)?;
-    if ip_address.enriched {
-        let ip_data: IPData = IPData {
-            cf_connecting_ip: Option::from(ip_address.ip.clone()),
-            x_real_ip: None,
-            x_forwarded_for: None,
-            cf_ipcountry: None,
-            ip_api_is_response: None,
-            ip_geolocate_response: Some(LocatorDe {
-                ip: ip_address.ip,
-                latitude: ip_address.latitude.unwrap_or_default().to_string(),
-                longitude: ip_address.longitude.unwrap_or_default().to_string(),
-                city: ip_address.city.unwrap_or_default().to_string(),
-                region: ip_address.region.unwrap_or_default().to_string(),
-                country: ip_address.country.unwrap_or_default().to_string(),
-                timezone: ip_address.timezone.unwrap_or_default().to_string(),
-                isp: ip_address.isp.unwrap_or_default().to_string(),
-            }),
-        };
-        enrich_uptime_report(&mut transaction, job.uptime_id, ip_data).await?;
-    } else {
+    if !ip_address.enriched {
         let ip_data = client
             .post(BLOCK_MESH_IP_WORKER)
             .json(&IpDataPostRequest { ip: job.ip })
@@ -106,7 +82,6 @@ pub async fn enrich_ip_and_cleanup(
             .json::<IPData>()
             .await?;
         enrich_ip_address(&mut transaction, ip_address.id, &ip_data).await?;
-        enrich_uptime_report(&mut transaction, job.uptime_id, ip_data).await?;
     }
     transaction.commit().await?;
     Ok(())
