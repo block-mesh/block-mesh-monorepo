79f058dd1b54f29af4f61b0c61574d19f31ad8ea
ohaddahan <ohaddahan@gmail.com>
2024-08-22 12:23:15 +0300

    touch up of landing page

--- libs/cloudflare-landing-page/src/lib.rs
+++ libs/cloudflare-landing-page/src/lib.rs
@@ -1,4 +1,5 @@
 use askama::Template;
+use serde_json::Value;
 use tracing_subscriber::fmt::format::Pretty;
 use tracing_subscriber::fmt::time::UtcTime;
 use tracing_subscriber::prelude::*;
@@ -28,6 +29,7 @@ fn start() {
 #[allow(dead_code)]
 #[template(path = "home.html")]
 struct Home {
+    pub counter: u64,
     pub chrome_extension_link: String,
     pub app_server: String,
     pub github: String,
@@ -40,20 +42,45 @@ struct Home {
 }
 
 #[event(fetch)]
-async fn main(_req: Request, _env: Env, ctx: Context) -> Result<Response> {
-    // let counter = ctx.kv("ab_testing")
-    let response = Home {
-        chrome_extension_link: BLOCK_MESH_CHROME_EXTENSION_LINK.to_string(),
-        app_server: BLOCK_MESH_APP_SERVER.to_string(),
-        github: BLOCK_MESH_GITHUB.to_string(),
-        twitter: BLOCK_MESH_TWITTER.to_string(),
-        gitbook: BLOCK_MESH_GITBOOK.to_string(),
-        logo: BLOCK_MESH_LOGO.to_string(),
-        // image: BLOCK_MESH_LANDING_PAGE_IMAGE.to_string(),
-        support: BLOCK_MESH_SUPPORT_EMAIL.to_string(),
-        chat: BLOCK_MESH_SUPPORT_CHAT.to_string(),
-    }
-    .render()
-    .unwrap();
-    Response::from_html(response)
+async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
+    let router = Router::new();
+    router
+        .get_async("/", |_req, ctx| async move {
+            let counter = ctx.kv("ab_testing")?.get("counter").json::<Value>().await?;
+            let counter = if counter.is_none() {
+                let value = serde_json::to_string(&Value::from(0))?;
+                ctx.kv("ab_testing")?
+                    .put("counter", value)?
+                    .execute()
+                    .await?;
+                0
+            } else {
+                let c = counter.unwrap().as_u64().unwrap();
+                let value = serde_json::to_string(&Value::from(c + 1))?;
+                ctx.kv("ab_testing")?
+                    .put("counter", value)?
+                    .execute()
+                    .await?;
+                c + 1
+            };
+
+            console_log!("counter = {:?}", counter);
+            let response = Home {
+                counter,
+                chrome_extension_link: BLOCK_MESH_CHROME_EXTENSION_LINK.to_string(),
+                app_server: BLOCK_MESH_APP_SERVER.to_string(),
+                github: BLOCK_MESH_GITHUB.to_string(),
+                twitter: BLOCK_MESH_TWITTER.to_string(),
+                gitbook: BLOCK_MESH_GITBOOK.to_string(),
+                logo: BLOCK_MESH_LOGO.to_string(),
+                // image: BLOCK_MESH_LANDING_PAGE_IMAGE.to_string(),
+                support: BLOCK_MESH_SUPPORT_EMAIL.to_string(),
+                chat: BLOCK_MESH_SUPPORT_CHAT.to_string(),
+            }
+            .render()
+            .unwrap();
+            Response::from_html(response)
+        })
+        .run(req, env)
+        .await
 }
