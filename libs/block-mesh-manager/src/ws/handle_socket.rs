8602a0d170bd5a16edacb45f3c2c7390a7b4edcf
Adimac93 <adimac93@gmail.com>
2024-08-30 09:37:14 +0200

    WS broadcast demo

--- libs/block-mesh-manager/src/ws/handle_socket.rs
+++ libs/block-mesh-manager/src/ws/handle_socket.rs
@@ -6,6 +6,7 @@ use crate::ws::process_message::process_message;
 use crate::ws::task_scheduler::TaskScheduler;
 use aws_sdk_sesv2::config::IntoShared;
 use axum::extract::ws::{Message, WebSocket};
+use block_mesh_common::constants::DeviceType;
 use block_mesh_common::interfaces::server_api::GetTaskResponse;
 use block_mesh_common::interfaces::ws_api::{WsMessage, WsMessageTypes};
 use futures::stream::SplitSink;
@@ -25,34 +26,20 @@ pub async fn handle_socket(
     state: Arc<AppState>,
     email: String,
 ) {
+    let (mut sender, mut receiver) = socket.split();
+
     let ws_connection_manager = state.ws_connection_manager.clone();
     let task_scheduler = ws_connection_manager.task_scheduler.clone();
     let broadcaster = ws_connection_manager.broadcaster.clone();
     let user_id = Uuid::new_v4();
     let mut broadcast_receiver = broadcaster.subscribe(user_id.clone()); // FIXME
 
-    tokio::task::spawn(async move {
-        while let Ok(msg) = broadcast_receiver.recv().await {
-            // handle broadcast
-        }
-    });
-    for i in 0..10 {
-        task_scheduler
-            .add_task(GetTaskResponse {
-                id: Uuid::new_v4(),
-                url: String::from(format!("https://example.com?={i}")),
-                method: String::from("GET"),
-                headers: None,
-                body: None,
-            })
-            .await;
-    }
+    broadcaster.broadcast(String::from("BROADCAST")).unwrap();
 
     let org_email = Arc::new(email);
     let notify = Arc::new(Notify::new());
     let notify_r = notify.clone();
-    let (mut sender, mut receiver) = socket.split();
-
+    let (sink_tx, mut sink_rx) = tokio::sync::mpsc::channel::<WsMessage>(10);
     let pool = state.pool.clone();
     let recv_task = tokio::spawn(async move {
         while let Some(Ok(msg)) = receiver.next().await {
@@ -93,6 +80,7 @@ pub async fn handle_socket(
             }
         }
     });
+    let task_sink_tx = sink_tx.clone();
     let send_task = tokio::spawn(async move {
         loop {
             let task_receiver = task_scheduler.add_session().await;
@@ -103,18 +91,41 @@ pub async fn handle_socket(
                 device: None,
                 message: WsMessageTypes::SendTaskFromServer(task),
             };
-            // tokio::time::sleep(std::time::Duration::from_secs(2)).await;
-            sender
-                .send(Message::Text(serde_json::to_string(&ws_message).unwrap()))
-                .await
-                .unwrap();
+            task_sink_tx.send(ws_message).await.unwrap();
             notify.notified().await;
         }
     });
 
+    let task_sink_tx = sink_tx.clone();
+    let broadcast_task = tokio::spawn(async move {
+        while let Ok(broadcast_message) = broadcast_receiver.recv().await {
+            tracing::info!("Broadcast received {broadcast_message}");
+            task_sink_tx
+                .send(WsMessage {
+                    message_id: Uuid::new_v4(),
+                    email: None,
+                    device: Some(DeviceType::Unknown),
+                    message: WsMessageTypes::SendUptimeFromServer,
+                })
+                .await
+                .unwrap();
+        }
+    });
+
+    let sink_task = tokio::spawn(async move {
+        while let Some(ws_message) = sink_rx.recv().await {
+            sender
+                .send(Message::Text(serde_json::to_string(&ws_message).unwrap()))
+                .await
+                .unwrap();
+        }
+    });
+
     tokio::select! {
-        o = recv_task => tracing::error!("recv_task task dead {:?}", o),
-        o = send_task => tracing::error!("send_task dead {:?}", o)
+        o = recv_task => tracing::error!("recv_task dead {:?}", o),
+        o = send_task => tracing::error!("send_task dead {:?}", o),
+        o = sink_task => tracing::error!("sink_task dead {:?}", o),
+        o = broadcast_task => tracing::error!("broadcast_task dead {:?}", o)
     }
 
     broadcaster.unsubscribe(&user_id);
