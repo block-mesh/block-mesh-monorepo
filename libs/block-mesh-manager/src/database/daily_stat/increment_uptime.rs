ebb4658750d54b71fc88dd7354e8b3b6c63b567f
ohaddahan <ohaddahan@gmail.com>
2024-09-02 15:27:25 +0300

    lint

--- libs/block-mesh-manager/src/database/daily_stat/increment_uptime.rs
+++ libs/block-mesh-manager/src/database/daily_stat/increment_uptime.rs
@@ -46,7 +46,7 @@ pub async fn update_users_ip_bulk(
         if ip.is_empty() {
             continue;
         }
-        let ip_address = get_or_create_ip_address(&mut transaction, ip).await;
+        let ip_address = get_or_create_ip_address(transaction, ip).await;
         if let Ok(ip_address) = ip_address {
             let _ = get_or_create_users_ip(transaction, pair.0, &ip_address.id).await;
         }
