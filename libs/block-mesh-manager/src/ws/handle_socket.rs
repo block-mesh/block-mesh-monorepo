e8d459a054296c95933fad5f78aabbb11b275af0
Adimac93 <adimac93@gmail.com>
2024-09-02 11:25:42 +0200

    encapsulate WS messenger & receiver

--- libs/block-mesh-manager/src/ws/handle_socket.rs
+++ libs/block-mesh-manager/src/ws/handle_socket.rs
@@ -1,28 +1,22 @@
 use crate::startup::application::AppState;
 use crate::ws::process_message::process_message;
 use axum::extract::ws::{Message, WebSocket};
-use block_mesh_common::interfaces::ws_api::WsServerMessage;
+use block_mesh_common::interfaces::ws_api::{WsClientMessage, WsServerMessage};
+use futures::stream::{SplitSink, SplitStream};
 use futures::{SinkExt, StreamExt};
 use std::net::SocketAddr;
 use std::ops::ControlFlow;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use tokio::sync::Notify;
+use tokio::task::JoinHandle;
 use uuid::Uuid;
 
-/// Actual websocket statemachine (one will be spawned per connection)
-pub async fn handle_socket(
-    socket: WebSocket,
-    who: SocketAddr,
-    state: Arc<AppState>,
-    _email: String,
-    user_id: Uuid,
-) {
-    let is_closing = Arc::new(AtomicBool::new(false));
-    let (mut ws_sink, mut ws_stream) = socket.split();
+fn messenger(
+    mut ws_sink: SplitSink<WebSocket, Message>,
+    is_cls: Arc<AtomicBool>,
+) -> (JoinHandle<()>, tokio::sync::mpsc::Sender<WsServerMessage>) {
     let (sink_tx, mut sink_rx) = tokio::sync::mpsc::channel::<WsServerMessage>(10);
-
-    let is_cls = is_closing.clone();
     let sink_task = tokio::spawn(async move {
         while let Some(server_message) = sink_rx.recv().await {
             let Ok(serialized_server_message) = serde_json::to_string(&server_message) else {
@@ -38,6 +32,42 @@ pub async fn handle_socket(
             }
         }
     });
+    (sink_task, sink_tx)
+}
+
+fn receiver(
+    mut ws_stream: SplitStream<WebSocket>,
+    is_cls: Arc<AtomicBool>,
+    who: SocketAddr,
+    task_scheduler_notifier: Arc<Notify>,
+) -> JoinHandle<()> {
+    tokio::spawn(async move {
+        while let Some(Ok(msg)) = ws_stream.next().await {
+            let control_flow = process_message(msg, who, task_scheduler_notifier.clone());
+            match control_flow {
+                ControlFlow::Continue(_) => {}
+                ControlFlow::Break(_) => {
+                    is_cls.store(true, Ordering::Relaxed);
+                    return;
+                }
+            }
+        }
+    })
+}
+
+/// Actual websocket statemachine (one will be spawned per connection)
+pub async fn handle_socket(
+    socket: WebSocket,
+    who: SocketAddr,
+    state: Arc<AppState>,
+    _email: String,
+    user_id: Uuid,
+) {
+    let is_closing = Arc::new(AtomicBool::new(false));
+    let (ws_sink, mut ws_stream) = socket.split();
+    let (sink_task, sink_tx) = messenger(ws_sink, is_closing.clone());
+    let notify = Arc::new(Notify::new());
+    let recv_task = receiver(ws_stream, is_closing.clone(), who, notify.clone());
 
     let ws_connection_manager = state.ws_connection_manager.clone();
     let task_scheduler = ws_connection_manager.task_scheduler;
@@ -62,23 +92,6 @@ pub async fn handle_socket(
     //         .await;
     // }
 
-    let notify = Arc::new(Notify::new());
-
-    let is_cls = is_closing.clone();
-    let task_scheduler_notifier = notify.clone();
-    let recv_task = tokio::spawn(async move {
-        while let Some(Ok(msg)) = ws_stream.next().await {
-            let control_flow = process_message(msg, who, task_scheduler_notifier.clone());
-            match control_flow {
-                ControlFlow::Continue(_) => {}
-                ControlFlow::Break(_) => {
-                    is_cls.store(true, Ordering::Relaxed);
-                    return;
-                }
-            }
-        }
-    });
-
     let _task_sink_tx = sink_tx.clone();
     let is_cls = is_closing.clone();
     let send_task = tokio::spawn(async move {
@@ -97,7 +110,7 @@ pub async fn handle_socket(
                 }
             }; // waits for new task
                // FIXME
-               // if let Err(_error) = task_sink_tx.send(WsServerMessage::AssignTask(task)).await {
+               // if let Err(_error) = _task_sink_tx.send(WsServerMessage::AssignTask(_task)).await {
                //     if is_cls.load(Ordering::Relaxed) {
                //         return;
                //     }
