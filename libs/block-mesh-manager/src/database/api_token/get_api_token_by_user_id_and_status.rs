07e82e660dd0df3f775771595648778f7cb83911
ohaddahan <ohaddahan@gmail.com>
2024-09-03 18:13:36 +0300

    remove txn from get-token

--- libs/block-mesh-manager/src/database/api_token/get_api_token_by_user_id_and_status.rs
+++ libs/block-mesh-manager/src/database/api_token/get_api_token_by_user_id_and_status.rs
@@ -1,6 +1,6 @@
 use crate::domain::api_token::{ApiToken, ApiTokenStatus};
 use secret::Secret;
-use sqlx::{Postgres, Transaction};
+use sqlx::{PgPool, Postgres, Transaction};
 use uuid::Uuid;
 
 #[tracing::instrument(
@@ -28,5 +28,33 @@ pub(crate) async fn get_api_token_by_usr_and_status(
         status.to_string()
     )
     .fetch_optional(&mut **transaction)
+    .await?)
+}
+
+#[tracing::instrument(
+    name = "Get API token by user and status",
+    skip(pool),
+    ret,
+    err,
+    level = "trace"
+)]
+pub(crate) async fn get_api_token_by_usr_and_status_pool(
+    pool: &PgPool,
+    user_id: &Uuid,
+    status: ApiTokenStatus,
+) -> anyhow::Result<Option<ApiToken>> {
+    Ok(sqlx::query_as!(
+        ApiToken,
+        r#"SELECT
+        id,
+        created_at,
+        user_id,
+        token as "token: Secret<Uuid>",
+        status as "status: ApiTokenStatus"
+        FROM api_tokens WHERE user_id = $1 and status = $2 LIMIT 1"#,
+        user_id,
+        status.to_string()
+    )
+    .fetch_optional(&*pool)
     .await?)
 }
