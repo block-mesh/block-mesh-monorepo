3c1c8b775f74539f5c97c8336dfba3f3ba3dcc60
ohaddahan <ohaddahan@gmail.com>
2024-09-03 20:10:50 +0300

    opt submit-bandwidth

--- libs/block-mesh-manager/src/routes/bandwidth/submit_bandwidth.rs
+++ libs/block-mesh-manager/src/routes/bandwidth/submit_bandwidth.rs
@@ -1,7 +1,7 @@
-use crate::database::aggregate::get_or_create_aggregate_by_user_and_name::get_or_create_aggregate_by_user_and_name;
-use crate::database::api_token::find_token::find_token;
+use crate::database::aggregate::get_or_create_aggregate_by_user_and_name::get_or_create_aggregate_by_user_and_name_pool;
+use crate::database::api_token::find_token::find_token_pool;
 use crate::database::bandwidth::delete_bandwidth_reports_by_time::delete_bandwidth_reports_by_time;
-use crate::database::user::get_user_by_id::get_user_opt_by_id;
+use crate::database::user::get_user_by_id::get_user_opt_by_id_pool;
 use crate::domain::aggregate::AggregateName;
 use crate::errors::error::Error;
 use crate::startup::application::AppState;
@@ -21,11 +21,10 @@ pub async fn handler(
     State(state): State<Arc<AppState>>,
     Json(body): Json<ReportBandwidthRequest>,
 ) -> Result<Json<ReportBandwidthResponse>, Error> {
-    let mut transaction = pool.begin().await.map_err(Error::from)?;
-    let api_token = find_token(&mut transaction, &body.api_token)
+    let api_token = find_token_pool(&pool, &body.api_token)
         .await?
         .ok_or(Error::ApiTokenNotFound)?;
-    let user = get_user_opt_by_id(&mut transaction, &api_token.user_id)
+    let user = get_user_opt_by_id_pool(&pool, &api_token.user_id)
         .await?
         .ok_or_else(|| Error::UserNotFound)?;
     if user.email.to_ascii_lowercase() != body.email.to_ascii_lowercase() {
@@ -42,20 +41,16 @@ pub async fn handler(
         .as_f64()
         .unwrap_or_default();
 
-    let download = get_or_create_aggregate_by_user_and_name(
-        &mut transaction,
-        AggregateName::Download,
-        user.id,
-    )
-    .await?;
+    let download =
+        get_or_create_aggregate_by_user_and_name_pool(&pool, AggregateName::Download, user.id)
+            .await?;
     let upload =
-        get_or_create_aggregate_by_user_and_name(&mut transaction, AggregateName::Upload, user.id)
+        get_or_create_aggregate_by_user_and_name_pool(&pool, AggregateName::Upload, user.id)
             .await?;
 
     let latency =
-        get_or_create_aggregate_by_user_and_name(&mut transaction, AggregateName::Latency, user.id)
+        get_or_create_aggregate_by_user_and_name_pool(&pool, AggregateName::Latency, user.id)
             .await?;
-    transaction.commit().await.map_err(Error::from)?;
 
     let _ = state
         .tx_aggregate_agg
