f1205ff45a77e45d1f5c3bf201a499b7c6dad101
ohaddahan <ohaddahan@gmail.com>
2024-09-03 17:54:15 +0300

    Remove ws manager for now (#473)
    * step by step
    
    * step by step

--- libs/block-mesh-manager/src/startup/application.rs
+++ libs/block-mesh-manager/src/startup/application.rs
@@ -12,7 +12,6 @@ use crate::startup::routers::static_un_auth_router::get_static_un_auth_router;
 use crate::startup::routers::ws_router::get_ws_router;
 use crate::worker::analytics_agg::AnalyticsMessage;
 use crate::worker::db_cleaner_cron::EnrichIp;
-use crate::ws::connection_manager::ConnectionManager;
 use axum::{Extension, Router};
 use axum_login::login_required;
 use block_mesh_common::feature_flag_client::FlagValue;
@@ -55,7 +54,7 @@ pub struct AppState {
     pub flags: HashMap<String, FlagValue>,
     pub cleaner_tx: Sender<EnrichIp>,
     pub redis: MultiplexedConnection,
-    pub ws_connection_manager: ConnectionManager,
+    // pub ws_connection_manager: ConnectionManager,
     pub tx_users_ip_agg: Sender<UsersIpMessage>,
     pub tx_aggregate_agg: Sender<AggregateMessage>,
 }
