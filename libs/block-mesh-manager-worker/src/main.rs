e1b7cce2e06fd4e6d62687ae15d51f0dcc3851c9
ohaddahan <ohaddahan@gmail.com>
2024-09-05 12:04:05 +0300

    CI

--- libs/block-mesh-manager-worker/src/main.rs
+++ libs/block-mesh-manager-worker/src/main.rs
@@ -1,7 +1,8 @@
-// use crate::pg_listener::{start_listening, Payload};
-// use block_mesh_common::constants::BLOCKMESH_PG_NOTIFY;
+use crate::pg_listener::{start_listening, Payload};
+use block_mesh_common::constants::BLOCKMESH_PG_NOTIFY;
 use block_mesh_common::env::load_dotenv::load_dotenv;
 use logger_general::tracing::setup_tracing_stdout_only;
+use std::env;
 
 mod pg_listener;
 
@@ -9,17 +10,14 @@ mod pg_listener;
 async fn main() -> anyhow::Result<()> {
     load_dotenv();
     setup_tracing_stdout_only();
-    // let db_pool = get_connection_pool(&configuration.database, Option::from(database_url)).await?;
-    // let redis_client = redis::Client::open(env::var("REDIS_URL")?)?;
-    // let redis = redis_client.get_multiplexed_async_connection().await?;
-    //
-    // let call_back = |payload: Payload| {
-    //     tracing::info!("Payload received: {:#?}", payload);
-    // };
-    // let db_listen = tokio::spawn(start_listening(
-    //     db_pool.clone(),
-    //     vec![BLOCKMESH_PG_NOTIFY],
-    //     call_back,
-    // ));
+    tracing::info!("Starting worker");
+    let db_pool = sqlx::PgPool::connect(&env::var("DATABASE_URL")?).await?;
+    let redis_client = redis::Client::open(env::var("REDIS_URL")?)?;
+    let _redis = redis_client.get_multiplexed_async_connection().await?;
+
+    let call_back = |payload: Payload| {
+        tracing::info!("Payload received: {:#?}", payload);
+    };
+    start_listening(db_pool.clone(), vec![BLOCKMESH_PG_NOTIFY], call_back).await?;
     Ok(())
 }
