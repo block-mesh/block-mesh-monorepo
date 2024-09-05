46c27c5e6bb35881c9ef5d0b790baeaed1d2bc1d
ohaddahan <ohaddahan@gmail.com>
2024-09-05 11:12:21 +0300

    refactor commons

--- libs/block-mesh-manager-worker/src/main.rs
+++ libs/block-mesh-manager-worker/src/main.rs
@@ -1,12 +1,14 @@
-// use crate::pg_listener::{start_listening, Payload};
-// use block_mesh_common::constants::BLOCKMESH_PG_NOTIFY;
+use crate::pg_listener::{start_listening, Payload};
+use block_mesh_common::constants::BLOCKMESH_PG_NOTIFY;
+use block_mesh_common::env::load_dotenv::load_dotenv;
+use logger_general::tracing::setup_tracing_stdout_only;
 
 mod pg_listener;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // load_dotenv();
-    // setup_tracing_stdout_only();
+    load_dotenv();
+    setup_tracing_stdout_only();
     // let db_pool = get_connection_pool(&configuration.database, Option::from(database_url)).await?;
     // let redis_client = redis::Client::open(env::var("REDIS_URL")?)?;
     // let redis = redis_client.get_multiplexed_async_connection().await?;
