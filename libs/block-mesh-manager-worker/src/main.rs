53aa072d2d2c675f1413025113cef8b369271e11
ohaddahan <ohaddahan@gmail.com>
2024-09-05 10:49:33 +0300

    start pg-listener-worker

---
+++ libs/block-mesh-manager-worker/src/main.rs
@@ -0,0 +1,24 @@
+use crate::pg_listener::{start_listening, Payload};
+use block_mesh_common::constants::BLOCKMESH_PG_NOTIFY;
+
+mod pg_listener;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    load_dotenv();
+    setup_tracing_stdout_only();
+    let db_pool = get_connection_pool(&configuration.database, Option::from(database_url)).await?;
+    let redis_client = redis::Client::open(env::var("REDIS_URL")?)?;
+    let redis = redis_client.get_multiplexed_async_connection().await?;
+
+    let call_back = |payload: Payload| {
+        tracing::info!("Payload received: {:#?}", payload);
+    };
+    let db_listen = tokio::spawn(start_listening(
+        db_pool.clone(),
+        vec![BLOCKMESH_PG_NOTIFY],
+        call_back,
+    ));
+
+    Ok(())
+}
