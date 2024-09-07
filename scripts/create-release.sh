16f1a9ecc0c62957fa2fab93eec5360400b1ba9c
ohaddahan <ohaddahan@gmail.com>
2024-09-07 17:42:00 +0300

    script

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -5,10 +5,11 @@ export ROOT="$(git rev-parse --show-toplevel)"
 export CARGO_TARGET_DIR="${ROOT}/target/PRE-PUSH"
 git checkout master
 git pull
-git checkout release
-git pull -Xtheirs
-git merge master -Xtheirs
+git checkout -b release
 #git branch --set-upstream-to=origin/release release
+git pull
+git merge master
+git rebase master -Xtheirs
 export VERSION=$(grep -m 1 '^version' Cargo.toml | sed -e 's/^version\s*=\s*//' | sed -e 's/"//g')
 export MINOR=$(echo $VERSION | cut -d '.' -f 3)
 export NEWMINOR=$(expr $MINOR + 1)
