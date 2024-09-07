5b8b9e851cded4358ff4653c8f6e4bcc66753ef4
ohaddahan <ohaddahan@gmail.com>
2024-09-08 00:08:39 +0300

    worker rx verbose (#510)
    * worker rx verbose
    
    * worker rx verbose

--- libs/block-mesh-manager-worker/src/call_backs/send_to_rx.rs
+++ libs/block-mesh-manager-worker/src/call_backs/send_to_rx.rs
@@ -3,6 +3,5 @@ use std::sync::Arc;
 use tokio::sync::broadcast::Sender;
 
 pub async fn send_to_rx(payload: Value, tx: Arc<Sender<Value>>) {
-    tracing::info!("Payload received: {:#?}", payload);
     let _ = tx.send(payload);
 }
---
+++ libs/block-mesh-manager/migrations/20240907210636_remove-duplicate-tasks-index.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS tasks_assigned_user_id_idx;
+DROP INDEX IF EXISTS tasks_assigned_user_id;
