0d141122f9cac2b0b824c5cb249f46c98620fe67
ohaddahan <ohaddahan@gmail.com>
2024-09-07 17:50:28 +0300

    Release (#495)
    * script
    
    * fml
    
    * fml
    
    * new release
    
    * script updates
    
    * script updates

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -x
+export ROOT="$(git rev-parse --show-toplevel)"
+export CARGO_TARGET_DIR="${ROOT}/target/PRE-PUSH"
 git branch -D release
 git branch -d release
 set -eo pipefail
 git checkout master
 git pull
 git checkout -b release
-git branch --set-upstream-to=origin/release release
-git pull
+#git branch --set-upstream-to=origin/release release
 git merge master
 git rebase master -Xtheirs
 export VERSION=$(grep -m 1 '^version' Cargo.toml | sed -e 's/^version\s*=\s*//' | sed -e 's/"//g')
@@ -15,7 +16,7 @@ export MINOR=$(echo $VERSION | cut -d '.' -f 3)
 export NEWMINOR=$(expr $MINOR + 1)
 export NEWVERSION=$(echo $VERSION | sed -e "s/$MINOR/$NEWMINOR/")
 sed -i -e "s/$VERSION/$NEWVERSION/" Cargo.toml
-cargo clippy --features ssr -p block-mesh-manager
+cargo clippy --all --features ssr,hydrate -- -D warnings
 git add Cargo.toml Cargo.lock
 git commit -m "New release ${NEWVERSION}"
 git push
