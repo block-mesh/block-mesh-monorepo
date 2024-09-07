acbc6b708da106a0f6571c36fdef433922c19c1f
ohaddahan <ohaddahan@gmail.com>
2024-09-07 17:53:20 +0300

    fml

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -10,7 +10,6 @@ export CARGO_TARGET_DIR="${ROOT}/target/PRE-PUSH"
 git checkout master
 git pull
 git checkout -b release
-#git branch --set-upstream-to=origin/release release
 git merge master
 git rebase master -Xtheirs
 export VERSION=$(grep -m 1 '^version' Cargo.toml | sed -e 's/^version\s*=\s*//' | sed -e 's/"//g')
@@ -19,6 +18,7 @@ export NEWMINOR=$(expr $MINOR + 1)
 export NEWVERSION=$(echo $VERSION | sed -e "s/$MINOR/$NEWMINOR/")
 sed -i -e "s/$VERSION/$NEWVERSION/" Cargo.toml
 cargo clippy --all --features ssr,hydrate -- -D warnings
+git branch --set-upstream-to=origin/release release
 git add Cargo.toml Cargo.lock
 git commit -m "New release ${NEWVERSION}"
 git push
