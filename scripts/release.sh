213f1edc4507f26f86c7199ae789968674400adc
ohaddahan <ohaddahan@gmail.com>
2024-09-07 15:24:28 +0300

    release branch (#489)
    * release branch
    
    * update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,12 +3,12 @@ set -x
 set -eo pipefail
 
 heroku container:login
-docker pull blockmesh/block-mesh-manager:latest
-docker tag blockmesh/block-mesh-manager:latest registry.heroku.com/block-mesh-manager/web
+docker pull blockmesh/block-mesh-manager:latest-amd64
+docker tag blockmesh/block-mesh-manager:latest-amd64 registry.heroku.com/block-mesh-manager/web
 docker push registry.heroku.com/block-mesh-manager/web
 #heroku container:push web -a block-mesh-manager
-docker pull blockmesh/block-mesh-manager-worker:latest
-docker tag blockmesh/block-mesh-manager-worker:latest registry.heroku.com/block-mesh-manager/worker
+docker pull blockmesh/block-mesh-manager-worker:latest-amd64
+docker tag blockmesh/block-mesh-manager-worker:latest-amd64 registry.heroku.com/block-mesh-manager/worker
 docker push registry.heroku.com/block-mesh-manager/worker
 heroku container:release web worker -a block-mesh-manager
 heroku restart -a block-mesh-manager
