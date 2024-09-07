6205cf10827ec67a20fe8c3259321c3932ef05d2
ohaddahan <ohaddahan@gmail.com>
2024-09-07 21:37:10 +0300

    fml

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -21,7 +21,7 @@ export NEWVERSION=$(echo $VERSION | sed -e "s/$MINOR/$NEWMINOR/")
 sed -i -e "s/$VERSION/$NEWVERSION/" Cargo.toml
 cargo clippy --all --features ssr,hydrate -- -D warnings
 git branch --set-upstream-to=origin/release release
-git pull
+#git pull
 git add Cargo.toml Cargo.lock
 git commit -m "New release ${NEWVERSION}"
 git push
