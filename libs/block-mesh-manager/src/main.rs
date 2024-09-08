864b5bcd53171b993841635a760fd759ffb14dab
ohaddahan <ohaddahan@gmail.com>
2024-09-08 13:52:57 +0300

    fix

--- libs/block-mesh-manager/src/main.rs
+++ libs/block-mesh-manager/src/main.rs
@@ -2,10 +2,10 @@
 #![deny(elided_lifetimes_in_paths)]
 #![deny(unreachable_pub)]
 
-use block_mesh_manager::worker::analytics_agg::analytics_agg;
 use cfg_if::cfg_if;
 
 cfg_if! { if #[cfg(feature = "ssr")] {
+    use block_mesh_manager::worker::analytics_agg::analytics_agg;
     use block_mesh_common::interfaces::db_messages::{
         AggregateMessage, AnalyticsMessage, DailyStatMessage,
     };
