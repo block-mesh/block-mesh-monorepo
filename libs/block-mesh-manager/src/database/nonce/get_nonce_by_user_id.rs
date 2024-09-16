56767a55e3d70eeb7077064cb55293a61e65dad7
ohaddahan <ohaddahan@gmail.com>
2024-09-16 23:03:19 +0300

    Idle in transaction (#538)
    * move to pool
    
    * move to pool
    
    * move to pool
    
    * move to pool
    
    * move to pool
    
    * move to pool

--- libs/block-mesh-manager/src/database/nonce/get_nonce_by_user_id.rs
+++ libs/block-mesh-manager/src/database/nonce/get_nonce_by_user_id.rs
@@ -1,6 +1,6 @@
 use crate::domain::nonce::Nonce;
 use secret::Secret;
-use sqlx::{Postgres, Transaction};
+use sqlx::{PgPool, Postgres, Transaction};
 use uuid::Uuid;
 
 #[tracing::instrument(name = "Get nonce", skip(transaction), ret, err, level = "trace")]
@@ -23,5 +23,28 @@ pub async fn get_nonce_by_user_id(
         user_id
     )
     .fetch_optional(&mut **transaction)
+    .await?)
+}
+
+#[tracing::instrument(name = "Get nonce via pool", skip_all, ret, err, level = "trace")]
+pub async fn get_nonce_by_user_id_pool(
+    pool: &PgPool,
+    user_id: &Uuid,
+) -> anyhow::Result<Option<Nonce>> {
+    Ok(sqlx::query_as!(
+        Nonce,
+        r#"
+        SELECT
+        id,
+        created_at,
+        user_id,
+        nonce as "nonce: Secret<String>"
+        FROM nonces
+        WHERE user_id = $1
+        ORDER BY created_at DESC
+        LIMIT 1"#,
+        user_id
+    )
+    .fetch_optional(pool)
     .await?)
 }
--- libs/block-mesh-manager/src/routes/emails/email_confirm.rs
+++ libs/block-mesh-manager/src/routes/emails/email_confirm.rs
@@ -1,4 +1,4 @@
-use crate::database::nonce::get_nonce_by_nonce::get_nonce_by_nonce;
+use crate::database::nonce::get_nonce_by_nonce::get_nonce_by_nonce_pool;
 use crate::database::user::get_user_by_id::get_user_opt_by_id;
 use crate::database::user::update_verified_email::update_verified_email;
 use crate::errors::error::Error;
@@ -16,8 +16,8 @@ pub async fn handler(
     Query(query): Query<ConfirmEmailRequest>,
 ) -> Result<Redirect, Error> {
     let mut transaction = pool.begin().await.map_err(Error::from)?;
-    let nonce = get_nonce_by_nonce(&mut transaction, &query.token).await?;
-    return match nonce {
+    let nonce = get_nonce_by_nonce_pool(&pool, &query.token).await?;
+    match nonce {
         None => Ok(Error::redirect(
             500,
             "Didn't find token",
@@ -55,5 +55,5 @@ pub async fn handler(
                 ))
             }
         }
-    };
+    }
 }
