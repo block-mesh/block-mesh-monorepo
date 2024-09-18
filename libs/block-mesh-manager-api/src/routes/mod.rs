51580694b5da3fc1a6fd3d0a1fde5b647763cd98
ohaddahan <ohaddahan@gmail.com>
2024-09-18 16:30:18 +0300

    fall back 200

--- libs/block-mesh-manager-api/src/routes/mod.rs
+++ libs/block-mesh-manager-api/src/routes/mod.rs
@@ -1,3 +1,4 @@
 pub mod check_token;
 pub mod get_token;
+pub mod ok;
 pub mod router;
--- libs/block-mesh-manager-api/src/routes/router.rs
+++ libs/block-mesh-manager-api/src/routes/router.rs
@@ -1,10 +1,11 @@
 use crate::routes::check_token::check_token;
 use crate::routes::get_token::get_token;
+use crate::routes::ok::ok_handler;
 use axum::routing::post;
 use axum::Router;
 
 pub fn get_router() -> Router {
     Router::new()
-        .route("/api/check_token", post(check_token))
-        .route("/api/get_token", post(get_token))
+        .route("/api/check_token", post(check_token).get(ok_handler))
+        .route("/api/get_token", post(get_token).get(ok_handler))
 }
