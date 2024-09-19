227fc9f42809e98a51f3cb43eef975e53a6c14eb
Adimac93 <adimac93@gmail.com>
2024-09-19 14:13:58 +0200

    add sentry tracing layer

--- libs/block-mesh-manager-api/src/main.rs
+++ libs/block-mesh-manager-api/src/main.rs
@@ -1,14 +1,16 @@
 use crate::routes::check_token::CheckTokenResponseMap;
 use crate::routes::get_token::GetTokenResponseMap;
 use crate::routes::router::get_router;
+use axum::extract::Request;
 use axum::{Extension, Router};
 use block_mesh_common::env::load_dotenv::load_dotenv;
-use logger_general::tracing::setup_tracing_stdout_only;
+use logger_general::tracing::setup_tracing_stdout_only_with_sentry;
+use sentry_tower::NewSentryLayer;
 use std::collections::HashMap;
-use std::env;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use std::time::Duration;
+use std::{env, mem};
 use tokio::net::TcpListener;
 use tokio::sync::Mutex;
 
@@ -18,16 +20,49 @@ mod routes;
 use tower_http::cors::CorsLayer;
 use tower_http::timeout::TimeoutLayer;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<()> {
     load_dotenv();
-    setup_tracing_stdout_only();
-    let db_pool = sqlx::PgPool::connect(&env::var("DATABASE_URL")?).await?;
+    let sentry_layer = env::var("SENTRY_LAYER")
+        .unwrap_or("false".to_string())
+        .parse()
+        .unwrap_or(false);
+    let sentry_url = env::var("SENTRY").unwrap_or_default();
+    let sentry_sample_rate = env::var("SENTRY_SAMPLE_RATE")
+        .unwrap_or("0.1".to_string())
+        .parse()
+        .unwrap_or(0.1);
+    if sentry_layer {
+        let _guard = sentry::init((
+            sentry_url,
+            sentry::ClientOptions {
+                debug: true,
+                sample_rate: sentry_sample_rate,
+                traces_sample_rate: sentry_sample_rate,
+                release: sentry::release_name!(),
+                ..Default::default()
+            },
+        ));
+        mem::forget(_guard);
+    }
+
+    setup_tracing_stdout_only_with_sentry();
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async { run(sentry_layer).await });
+    Ok(())
+}
+
+async fn run(is_with_sentry: bool) {
+    let db_pool = sqlx::PgPool::connect(&env::var("DATABASE_URL").unwrap())
+        .await
+        .unwrap();
     let router = get_router();
     let check_token_map: CheckTokenResponseMap = Arc::new(Mutex::new(HashMap::new()));
     let get_token_map: GetTokenResponseMap = Arc::new(Mutex::new(HashMap::new()));
     let cors = CorsLayer::permissive();
-    let app = Router::new()
+    let mut app = Router::new()
         .nest("/", router)
         .layer(Extension(db_pool.clone()))
         .layer(Extension(check_token_map))
@@ -39,13 +74,19 @@ async fn main() -> anyhow::Result<()> {
                 .parse()
                 .unwrap_or(1000),
         )));
+    if is_with_sentry {
+        app = app.layer(NewSentryLayer::<Request>::new_from_top());
+    }
     let port = env::var("PORT").unwrap_or("8001".to_string());
-    let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
-    tracing::info!("Listening on {}", listener.local_addr()?);
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", port))
+        .await
+        .unwrap();
+    tracing::info!("Listening on {}", listener.local_addr().unwrap());
+
     axum::serve(
         listener,
         app.into_make_service_with_connect_info::<SocketAddr>(),
     )
-    .await?;
-    Ok(())
+    .await
+    .unwrap();
 }
