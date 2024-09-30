31457f427c557ad54b3a607c37d1487c45f666b2
ohaddahan <ohaddahan@gmail.com>
2024-09-30 13:43:56 +0300

    update db calls

--- libs/block-mesh-manager/src/database/analytics/inserting_client_analytics.rs
+++ libs/block-mesh-manager/src/database/analytics/inserting_client_analytics.rs
@@ -8,6 +8,7 @@ pub(crate) async fn inserting_client_analytics(
     user_id: &Uuid,
     depin_aggregator: &str,
     device_type: &DeviceType,
+    version: &str,
 ) -> anyhow::Result<Uuid> {
     let now = Utc::now();
     let id = Uuid::new_v4();
@@ -15,9 +16,9 @@ pub(crate) async fn inserting_client_analytics(
         r#"
         INSERT
         INTO analytics
-        (user_id, depin_aggregator, device_type, created_at, updated_at, id)
+        (user_id, depin_aggregator, device_type, created_at, updated_at, id, version)
         VALUES
-        ($1, $2, $3, $4, $5, $6)
+        ($1, $2, $3, $4, $5, $6, $7)
         ON CONFLICT (user_id, depin_aggregator) DO UPDATE SET updated_at = $5
     "#,
         user_id,
@@ -25,7 +26,8 @@ pub(crate) async fn inserting_client_analytics(
         device_type.to_string(),
         now.clone(),
         now,
-        id
+        id,
+        version
     )
     .execute(&mut **transaction)
     .await?;
