61a32b438529ccbc03b8d711a17625032c7e4421
ohaddahan <ohaddahan@gmail.com>
2024-10-01 11:18:34 +0300

    add task limit

--- libs/block-mesh-manager/src/routes/tasks/get_task.rs
+++ libs/block-mesh-manager/src/routes/tasks/get_task.rs
@@ -9,6 +9,7 @@ use crate::errors::error::Error;
 use crate::middlewares::rate_limit::filter_request;
 use crate::middlewares::task_limit::TaskLimit;
 use crate::startup::application::AppState;
+use crate::utils::cache_envar::get_envar;
 use anyhow::Context;
 use axum::extract::State;
 use axum::{Extension, Json};
