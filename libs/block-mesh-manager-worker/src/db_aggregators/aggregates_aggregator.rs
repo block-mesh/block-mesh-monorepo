620c2ae6c58e93ccc6d9b83c4b08b3768e1727cd
ohaddahan <ohaddahan@gmail.com>
2024-10-06 08:43:57 +0300

    worker-shenanigans

--- libs/block-mesh-manager-worker/src/db_aggregators/aggregates_aggregator.rs
+++ libs/block-mesh-manager-worker/src/db_aggregators/aggregates_aggregator.rs
@@ -18,27 +18,34 @@ pub async fn aggregates_aggregator(
     let mut calls: HashMap<_, _> = HashMap::new();
     let mut count = 0;
     let mut prev = Utc::now();
-    while let Ok(message) = rx.recv().await {
-        if let Ok(message) = serde_json::from_value::<AggregateMessage>(message) {
-            calls.insert(message.id, message.value);
-            count += 1;
-            let now = Utc::now();
-            let diff = now - prev;
-            let run = diff.num_seconds() > time_limit || count >= agg_size;
-            prev = Utc::now();
-            if run {
-                tracing::info!("aggregates_aggregator starting txn");
-                if let Ok(mut transaction) = create_txn(&pool).await {
-                    for pair in calls.iter() {
-                        let _ = update_aggregate(&mut transaction, pair.0, pair.1).await;
+    loop {
+        match rx.recv().await {
+            Ok(message) => {
+                if let Ok(message) = serde_json::from_value::<AggregateMessage>(message) {
+                    calls.insert(message.id, message.value);
+                    count += 1;
+                    let now = Utc::now();
+                    let diff = now - prev;
+                    let run = diff.num_seconds() > time_limit || count >= agg_size;
+                    prev = Utc::now();
+                    if run {
+                        tracing::info!("aggregates_aggregator starting txn");
+                        if let Ok(mut transaction) = create_txn(&pool).await {
+                            for pair in calls.iter() {
+                                let _ = update_aggregate(&mut transaction, pair.0, pair.1).await;
+                            }
+                            let _ = commit_txn(transaction).await;
+                        }
+                        tracing::info!("aggregates_aggregator finished txn");
+                        count = 0;
+                        calls.clear();
                     }
-                    let _ = commit_txn(transaction).await;
                 }
-                tracing::info!("aggregates_aggregator finished txn");
-                count = 0;
-                calls.clear();
+            }
+            Err(e) => {
+                tracing::error!("aggregates_aggregator error recv: {:?}", e);
+                return Err(anyhow!("aggregates_aggregator error recv: {:?}", e));
             }
         }
     }
-    Err(anyhow!("aggregates_aggregator terminated"))
 }
