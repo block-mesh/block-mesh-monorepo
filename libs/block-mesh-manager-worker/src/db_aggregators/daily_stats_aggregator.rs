25c02ebb24310b196096da5ddb27d89f742b18ad
ohaddahan <ohaddahan@gmail.com>
2024-10-07 12:46:05 +0300

    worker touch up

--- libs/block-mesh-manager-worker/src/db_aggregators/daily_stats_aggregator.rs
+++ libs/block-mesh-manager-worker/src/db_aggregators/daily_stats_aggregator.rs
@@ -18,27 +18,34 @@ pub async fn daily_stats_aggregator(
     let mut calls: HashMap<_, _> = HashMap::new();
     let mut count = 0;
     let mut prev = Utc::now();
-    while let Ok(message) = rx.recv().await {
-        if let Ok(message) = serde_json::from_value::<DailyStatMessage>(message) {
-            calls.insert(message.id, message.uptime);
-            count += 1;
-            let now = Utc::now();
-            let diff = now - prev;
-            let run = diff.num_seconds() > time_limit || count >= agg_size;
-            prev = Utc::now();
-            if run {
-                tracing::info!("daily_stats_aggregator starting txn");
-                if let Ok(mut transaction) = create_txn(&pool).await {
-                    for pair in calls.iter() {
-                        let _ = increment_uptime(&mut transaction, pair.0, *pair.1).await;
+    loop {
+        match rx.recv().await {
+            Ok(message) => {
+                if let Ok(message) = serde_json::from_value::<DailyStatMessage>(message) {
+                    calls.insert(message.id, message.uptime);
+                    count += 1;
+                    let now = Utc::now();
+                    let diff = now - prev;
+                    let run = diff.num_seconds() > time_limit || count >= agg_size;
+                    prev = Utc::now();
+                    if run {
+                        tracing::info!("daily_stats_aggregator starting txn");
+                        if let Ok(mut transaction) = create_txn(&pool).await {
+                            for pair in calls.iter() {
+                                let _ = increment_uptime(&mut transaction, pair.0, *pair.1).await;
+                            }
+                            let _ = commit_txn(transaction).await;
+                        }
+                        count = 0;
+                        calls.clear();
+                        tracing::info!("daily_stats_aggregator finished txn");
                     }
-                    let _ = commit_txn(transaction).await;
                 }
-                count = 0;
-                calls.clear();
-                tracing::info!("daily_stats_aggregator finished txn");
+            }
+            Err(e) => {
+                tracing::error!("daily_stats_aggregator error recv: {:?}", e);
+                return Err(anyhow!("daily_stats_aggregator error recv: {:?}", e));
             }
         }
     }
-    Err(anyhow!("daily_stats_aggregator terminated"))
 }
