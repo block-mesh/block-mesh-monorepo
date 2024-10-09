35e76e0ad462bf5d533d8382d9ca67562d834d7e
ohaddahan <ohaddahan@gmail.com>
2024-10-09 10:43:03 +0300

    deploy-script-for-ws (#626)

---
+++ scripts/release/ws-block-mesh-manager.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+heroku container:login
+docker pull blockmesh/block-mesh-manager-ws:latest-amd64
+docker tag blockmesh/block-mesh-manager-ws:latest-amd64 registry.heroku.com/block-mesh-manager-ws/web
+docker push registry.heroku.com/block-mesh-manager-ws/web
+#heroku container:push web -a feature-flags-server
+heroku container:release web -a block-mesh-manager-ws
+heroku restart -a block-mesh-manager-ws
