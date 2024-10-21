9365ccf37a00667e131ec3d897ddd91f1695af82
ohaddahan <ohaddahan@gmail.com>
2024-10-21 20:01:43 +0300

    Add cargo to base

--- docker/blockmesh-ubuntu-base.Dockerfile
+++ docker/blockmesh-ubuntu-base.Dockerfile
@@ -2,3 +2,5 @@ FROM ubuntu:22.04
 ARG DEBIAN_FRONTEND=noninteractive
 RUN apt-get update
 RUN apt-get install curl gzip git-all -y
+RUN apt-get install build-essential -y
+RUN apt-get install cargo -y
--- scripts/docker/blockmesh-ubuntu-base.sh
+++ scripts/docker/blockmesh-ubuntu-base.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+export ROOT="$(git rev-parse --show-toplevel)"
+
 docker login
 
-docker buildx build --platform linux/amd64 -t blockmesh-ubuntu-base -f blockmesh-ubuntu-base.Dockerfile --load .
+docker buildx build --platform linux/amd64 -t blockmesh-ubuntu-base -f "${ROOT}/docker/blockmesh-ubuntu-base.Dockerfile" --load .
 docker tag blockmesh-ubuntu-base blockmesh/blockmesh-ubuntu-base:latest-amd64
 docker push blockmesh/blockmesh-ubuntu-base:latest-amd64
 
-docker buildx build --platform linux/arm64 -t blockmesh-ubuntu-base -f blockmesh-ubuntu-base.Dockerfile --load .
+docker buildx build --platform linux/arm64 -t blockmesh-ubuntu-base -f "${ROOT}/blockmesh-ubuntu-base.Dockerfile" --load .
 docker tag blockmesh-ubuntu-base blockmesh/blockmesh-ubuntu-base:latest-arm64
 docker push blockmesh/blockmesh-ubuntu-base:latest-arm64
