8e6bd2c408d6d0ca8de5ac6a4eb6b12d312444f4
ohaddahan <ohaddahan@gmail.com>
2024-10-24 07:19:54 +0300

    fix BW stats

--- libs/extension/extension_js/js/background.js
+++ libs/extension/extension_js/js/background.js
@@ -3,8 +3,8 @@
 import initWasmModule, {
   task_poller,
   report_uptime,
-  uptime_fetcher,
   start_websocket,
+  measure_bandwidth,
   stop_websocket
 } from './wasm/blockmesh_ext.js'
 
@@ -120,8 +120,8 @@ function recreate_intervals() {
   intervals.push(
     setInterval(async () => {
       await create_alarm().then(onSuccess, onError)
-      // await measure_bandwidth().then(onSuccess, onError) // TODO bring back, need to release this gradually
-    }, polling_interval + Math.random())
+      await measure_bandwidth().then(onSuccess, onError)
+    }, 10 * polling_interval + Math.random())
   )
 }
 
