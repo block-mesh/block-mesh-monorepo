0e8ea0047ab1330aebcb1eff58a504a3f418317b
ohaddahan <ohaddahan@gmail.com>
2024-10-27 11:49:55 +0200

    better init_db

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -x
+docker network create blockmesh_network
 set -eo pipefail
 export _PWD="$(pwd)"
 export ROOT="$(git rev-parse --show-toplevel)"
@@ -30,46 +31,53 @@ fi
 
 DB_USER="${POSTGRES_USER:=postgres}"
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-DB_NAME="${POSTGRES_DB:=block-mesh}"
-DB_PORT="${POSTGRES_PORT:=5559}"
+#DB_NAME="${POSTGRES_DB:=block-mesh}"
+#DB_PORT="${POSTGRES_PORT:=5559}"
 
-#docker network create blockmesh_network
+function remove_pg() {
+   DOCKERS="$(docker ps -a -q --filter ancestor=postgres:15.3-alpine3.18 --format="{{.ID}}")"
+    if [ -n "$DOCKERS" ]
+    then
+      ensure docker rm --force --volumes $DOCKERS
+    fi
+}
 
+function start_db() {
+  DB_NAME=$1
+  DB_PORT=$2
+    ensure docker run \
+     --network=blockmesh_network \
+      -e POSTGRES_USER=postgres \
+      -e POSTGRES_PASSWORD=password \
+      -e POSTGRES_DB=${DB_NAME} \
+      -e POSTGRES_PORT=${DB_PORT} \
+      -p "${DB_PORT}:${DB_PORT}" \
+      -d postgres:15.3-alpine3.18 \
+      postgres -p ${DB_PORT} -N 1000
+      export PGPASSWORD=password
+      until psql -h "localhost" -p "${DB_PORT}" -U "${DB_USER}" -d postgres -c '\q'
+      do
+        >&2 echo "Postgres is still unavailable - sleeping"
+        sleep 1
+      done
+}
 
 if [ "${SKIP_DOCKER}" != "yes" ]
 then
-  DOCKERS="$(docker ps -a -q --filter ancestor=postgres:15.3-alpine3.18 --format="{{.ID}}")"
+  remove_pg
+  start_db block-mesh 5559
+  start_db tg-bot 5551
+
+  DOCKERS="$(docker ps -a -q --filter ancestor=redis:alpine3.20 --format="{{.ID}}")"
   if [ -n "$DOCKERS" ]
   then
     ensure docker rm --force --volumes $DOCKERS
   fi
   ensure docker run \
-   --network=blockmesh_network \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -e POSTGRES_PORT=${DB_PORT} \
-    -p "${DB_PORT}":5559 \
-    -d postgres:15.3-alpine3.18 \
-    postgres -p ${DB_PORT} -N 1000
-
- DOCKERS="$(docker ps -a -q --filter ancestor=redis:alpine3.20 --format="{{.ID}}")"
- if [ -n "$DOCKERS" ]
- then
-  ensure docker rm --force --volumes $DOCKERS
- fi
- ensure docker run \
- --network=blockmesh_network \
- --name redis -p 6379:6379 -d redis:alpine3.20
- export REDIS_URL="redis://127.0.0.1:6379"
+    --network=blockmesh_network \
+    --name redis -p 6379:6379 -d redis:alpine3.20
+  export REDIS_URL="redis://127.0.0.1:6379"
 fi
-
-export PGPASSWORD="${DB_PASSWORD}"
-until psql -h "localhost" -p "${DB_PORT}" -U "${DB_USER}" -d postgres -c '\q'
-do
-    >&2 echo "Postgres is still unavailable - sleeping"
-    sleep 1
-done
 
 function migrate() {
   sqlx migrate run --source migrations --ignore-missing
@@ -78,34 +86,35 @@ function migrate() {
 
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
 set -x
-export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}"
+
+export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@localhost:5559/block-mesh"
 echo "create DB"
 ensure sqlx database create
-if [ "${DB_NAME}" = "block-mesh" ]; then
-  echo "migrate DB :79"
-  ensure migrate
-  cd "${ROOT}/libs/block-mesh-manager-worker" || exit
-  echo "migrate DB :83"
-  ensure migrate
-  cd "${ROOT}/libs/block-mesh-manager-api" || exit
-  echo "migrate DB :87"
-  ensure migrate
-  cd "${ROOT}/libs/block-mesh-manager-ws" || exit
-  echo "migrate DB :91"
-  ensure migrate
-  cd "${ROOT}/libs/feature-flags-server" || exit
-  echo "migrate DB :95"
-  ensure migrate
-  cd "${ROOT}/libs/block-mesh-manager-database-domain" || exit
-  echo "migrate DB :100"
-  ensure migrate
-  cd "${ROOT}/libs/database-utils" || exit
-  echo "migrate DB :103"
-  ensure migrate
-else
-  cd "${ROOT}/libs/tg-privacy-bot" || exit
-  echo "migrate DB :107"
-  ensure migrate
-fi
+echo "migrate DB block-mesh-manager"
+ensure migrate
+cd "${ROOT}/libs/block-mesh-manager-worker" || exit
+echo "migrate DB block-mesh-manager-worker"
+ensure migrate
+cd "${ROOT}/libs/block-mesh-manager-api" || exit
+echo "migrate DB block-mesh-manager-api"
+ensure migrate
+cd "${ROOT}/libs/block-mesh-manager-ws" || exit
+echo "migrate DB block-mesh-manager-ws"
+ensure migrate
+cd "${ROOT}/libs/feature-flags-server" || exit
+echo "migrate DB feature-flags-server"
+ensure migrate
+cd "${ROOT}/libs/block-mesh-manager-database-domain" || exit
+echo "migrate DB block-mesh-manager-database-domain"
+ensure migrate
+cd "${ROOT}/libs/database-utils" || exit
+echo "migrate DB database-utils"
+ensure migrate
+export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@localhost:5551/tg-bot"
+echo "create DB"
+ensure sqlx database create
+cd "${ROOT}/libs/tg-privacy-bot" || exit
+echo "migrate DB tg-privacy-bot"
+ensure migrate
 >&2 echo "Postgres has been migrated, ready to go!"
 cd "${_PWD}"
