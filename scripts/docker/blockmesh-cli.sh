53255d51375f82c76d55f1b95b29b7d500dd7cb9
ohaddahan <ohaddahan@gmail.com>
2024-11-01 15:24:23 +0200

    fml-scripts

--- scripts/docker/blockmesh-cli.sh
+++ scripts/docker/blockmesh-cli.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker buildx build --platform linux/arm64 -t bmesh -f blockmesh-cli.Dockerfile --load .
+docker buildx build --platform linux/arm64 -t bmesh -f docker/blockmesh-cli.Dockerfile --load .
+# docker run --platform linux/arm64 --entrypoint /bin/bash -it -t bmesh
