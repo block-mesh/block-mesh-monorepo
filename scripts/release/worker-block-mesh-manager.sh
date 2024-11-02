2e9d962991b5b9c7e267eacdb71a1eed09b0bef2
ohaddahan <ohaddahan@gmail.com>
2024-11-02 19:06:19 +0200

    remove -amd64 suffix

--- scripts/release/worker-block-mesh-manager.sh
+++ scripts/release/worker-block-mesh-manager.sh
@@ -4,8 +4,8 @@ set -eo pipefail
 
 heroku container:login
 #heroku container:push web -a block-mesh-manager
-docker pull blockmesh/block-mesh-manager-worker:latest-amd64
-docker tag blockmesh/block-mesh-manager-worker:latest-amd64 registry.heroku.com/block-mesh-manager-worker/web
+docker pull blockmesh/block-mesh-manager-worker:latest
+docker tag blockmesh/block-mesh-manager-worker:latest registry.heroku.com/block-mesh-manager-worker/web
 docker push registry.heroku.com/block-mesh-manager-worker/web
 heroku container:release web -a block-mesh-manager-worker
 heroku restart -a block-mesh-manager-worker
