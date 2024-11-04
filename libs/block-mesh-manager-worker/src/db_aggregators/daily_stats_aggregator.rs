6f07d47c32b419bb3aa0b1b15ff484d95eeda6a3
ohaddahan <ohaddahan@gmail.com>
2024-11-04 16:34:50 +0200

    Bulk actions

--- libs/block-mesh-manager-worker/src/db_aggregators/daily_stats_aggregator.rs
+++ libs/block-mesh-manager-worker/src/db_aggregators/daily_stats_aggregator.rs
@@ -1,16 +1,42 @@
 use anyhow::anyhow;
 use block_mesh_common::interfaces::db_messages::DailyStatMessage;
-use block_mesh_manager_database_domain::domain::increment_uptime::increment_uptime;
 use chrono::Utc;
 use database_utils::utils::instrument_wrapper::{commit_txn, create_txn};
+use flume::Sender;
 use serde_json::Value;
 use sqlx::PgPool;
 use std::collections::HashMap;
 use tokio::sync::broadcast::error::RecvError;
 use tokio::sync::broadcast::Receiver;
+use tokio::task::JoinHandle;
+use uuid::Uuid;
+
+pub fn daily_stats_create_bulk_query(calls: HashMap<Uuid, f64>) -> String {
+    let values: Vec<String> = calls
+        .iter()
+        .map(|(id, value)| format!("('{}'::uuid, {})", id, value))
+        .collect();
+    let value_str = values.join(",");
+
+    format!(
+        r#"
+WITH updates (id, value) AS (
+VALUES {}
+)
+-- Update statement using the CTE
+UPDATE daily_stats
+SET
+    uptime = uptime + updates.value
+FROM updates
+WHERE daily_stats.id = updates.id;
+    "#,
+        value_str
+    )
+}
 
 #[tracing::instrument(name = "daily_stats_aggregator", skip_all, err)]
 pub async fn daily_stats_aggregator(
+    joiner_tx: Sender<JoinHandle<()>>,
     pool: PgPool,
     mut rx: Receiver<Value>,
     agg_size: i32,
@@ -30,13 +56,17 @@ pub async fn daily_stats_aggregator(
                     let run = diff.num_seconds() > time_limit || count >= agg_size;
                     prev = Utc::now();
                     if run {
-                        tracing::info!("daily_stats_aggregator starting txn");
-                        if let Ok(mut transaction) = create_txn(&pool).await {
-                            for pair in calls.iter() {
-                                let _ = increment_uptime(&mut transaction, pair.0, *pair.1).await;
+                        let calls_clone = calls.clone();
+                        let poll_clone = pool.clone();
+                        let handle = tokio::spawn(async move {
+                            tracing::info!("daily_stats_aggregator starting txn");
+                            if let Ok(mut transaction) = create_txn(&poll_clone).await {
+                                let query = daily_stats_create_bulk_query(calls_clone);
+                                let _ = sqlx::query(&query).execute(&mut *transaction).await;
+                                let _ = commit_txn(transaction).await;
                             }
-                            let _ = commit_txn(transaction).await;
-                        }
+                        });
+                        let _ = joiner_tx.send_async(handle).await;
                         count = 0;
                         calls.clear();
                         tracing::info!("daily_stats_aggregator finished txn");
