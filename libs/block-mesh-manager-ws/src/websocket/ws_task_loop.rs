7b0c8c462c7e5b7762d4debca6e522453d607414
ohaddahan <ohaddahan@gmail.com>
2024-11-04 12:37:56 +0200

    lint

--- libs/block-mesh-manager-ws/src/websocket/ws_task_loop.rs
+++ libs/block-mesh-manager-ws/src/websocket/ws_task_loop.rs
@@ -23,7 +23,7 @@ pub async fn assign_tasks_to_users(
     mut redis: MultiplexedConnection,
     task_limit: u64,
     broadcaster: &Broadcaster,
-    mut transaction: &mut Transaction<'_, Postgres>,
+    transaction: &mut Transaction<'_, Postgres>,
     expire: u64,
 ) {
     loop {
