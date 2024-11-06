a8f618e610cb099c01c4bee7659f1e7ce4102fb4
ohaddahan <ohaddahan@gmail.com>
2024-11-06 10:17:39 +0200

    fix db lock due to partition (#714)

--- libs/block-mesh-manager-database-domain/src/domain/update_task_assigned.rs
+++ libs/block-mesh-manager-database-domain/src/domain/update_task_assigned.rs
@@ -6,19 +6,33 @@ use uuid::Uuid;
 pub async fn update_task_assigned(
     transaction: &mut Transaction<'_, Postgres>,
     task_id: Uuid,
-    user_id: Uuid,
+    assigned_user_id: Uuid,
     status: TaskStatus,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
+        // r#"
+        // UPDATE
+        // tasks
+        // SET
+        // assigned_user_id = $1,
+        // status = $2
+        // WHERE
+        // id = $3"#,
         r#"
-        UPDATE
-        tasks
-        SET
-        assigned_user_id = $1,
-        status = $2
-        WHERE
-        id = $3"#,
-        user_id,
+WITH locked_task AS (
+    SELECT id
+    FROM tasks
+    WHERE id = $3
+    FOR UPDATE SKIP LOCKED
+)
+UPDATE tasks
+SET
+    assigned_user_id = $1,
+    status = $2
+WHERE
+    id IN (SELECT id FROM locked_task)
+    "#,
+        assigned_user_id,
         status.to_string(),
         task_id,
     )
