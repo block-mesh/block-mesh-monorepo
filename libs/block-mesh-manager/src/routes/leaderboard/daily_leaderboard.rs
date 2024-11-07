ba87193f830633768619d3388c336f7a5fa8e3cf
ohaddahan <ohaddahan@gmail.com>
2024-11-07 09:15:30 +0200

    fml query

--- libs/block-mesh-manager/src/routes/leaderboard/daily_leaderboard.rs
+++ libs/block-mesh-manager/src/routes/leaderboard/daily_leaderboard.rs
@@ -1,6 +1,8 @@
+#[allow(unused_imports)]
 use crate::database::leaderboard::get_daily_leaderboard::get_daily_leaderboard;
 use crate::errors::error::Error;
 use crate::middlewares::authentication::Backend;
+#[allow(unused_imports)]
 use crate::utils::points::{TASKS_FACTOR, UPTIME_FACTOR};
 use axum::{Extension, Json};
 use axum_login::AuthSession;
@@ -14,27 +16,28 @@ pub async fn handler(
 ) -> Result<Json<DailyLeaderboard>, Error> {
     let mut transaction = pool.begin().await.map_err(Error::from)?;
     let day = Utc::now().date_naive() - Duration::days(1);
-    let user = auth.user.ok_or(Error::UserNotFound)?;
-    let leaderboard_users: Vec<LeaderBoardUser> =
-        get_daily_leaderboard(&mut transaction, UPTIME_FACTOR, TASKS_FACTOR, 5)
-            .await?
-            .into_iter()
-            .map(|i| {
-                if user.email == i.email {
-                    LeaderBoardUser {
-                        email: user.email.clone(),
-                        points: i.points,
-                        ips: i.ips,
-                    }
-                } else {
-                    LeaderBoardUser {
-                        email: "***@***".to_string(),
-                        points: i.points,
-                        ips: i.ips,
-                    }
-                }
-            })
-            .collect();
+    let _user = auth.user.ok_or(Error::UserNotFound)?;
+    let leaderboard_users: Vec<LeaderBoardUser> = vec![];
+    // let leaderboard_users: Vec<LeaderBoardUser> =
+    //     get_daily_leaderboard(&mut transaction, UPTIME_FACTOR, TASKS_FACTOR, 5)
+    //         .await?
+    //         .into_iter()
+    //         .map(|i| {
+    //             if user.email == i.email {
+    //                 LeaderBoardUser {
+    //                     email: user.email.clone(),
+    //                     points: i.points,
+    //                     ips: i.ips,
+    //                 }
+    //             } else {
+    //                 LeaderBoardUser {
+    //                     email: "***@***".to_string(),
+    //                     points: i.points,
+    //                     ips: i.ips,
+    //                 }
+    //             }
+    //         })
+    //         .collect();
     transaction.commit().await.map_err(Error::from)?;
     Ok(Json(DailyLeaderboard {
         leaderboard_users,
