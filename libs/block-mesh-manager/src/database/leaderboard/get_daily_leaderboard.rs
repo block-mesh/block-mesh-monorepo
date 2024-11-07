8f998791431f95d3d842ee3fd2ee272d089c784a
ohaddahan <ohaddahan@gmail.com>
2024-11-07 10:13:21 +0200

    Partition daily stats (#720)
    * change daily stats
    
    * change daily stats
    
    * change daily stats
    
    * change daily stats

--- libs/block-mesh-manager/src/database/leaderboard/get_daily_leaderboard.rs
+++ libs/block-mesh-manager/src/database/leaderboard/get_daily_leaderboard.rs
@@ -1,11 +1,6 @@
 use block_mesh_common::interfaces::server_api::LeaderBoardUser;
-use chrono::{Duration, NaiveDate, Utc};
-use dashmap::DashMap;
+use chrono::{Duration, Utc};
 use sqlx::{Postgres, Transaction};
-use std::sync::Arc;
-use tokio::sync::OnceCell;
-
-static QUERY_CACHE: OnceCell<Arc<DashMap<NaiveDate, Vec<LeaderBoardUser>>>> = OnceCell::const_new();
 
 pub async fn get_daily_leaderboard(
     transaction: &mut Transaction<'_, Postgres>,
@@ -13,36 +8,41 @@ pub async fn get_daily_leaderboard(
     tasks_factor: f64,
     limit: i64,
 ) -> anyhow::Result<Vec<LeaderBoardUser>> {
-    let query = QUERY_CACHE
-        .get_or_init(|| async { Arc::new(DashMap::new()) })
-        .await;
     let day = Utc::now().date_naive() - Duration::days(1);
-    if let Some(entry) = query.get(&day) {
-        return Ok(entry.clone());
-    }
     let daily_stats = sqlx::query_as!(
         LeaderBoardUser,
         r#"
         SELECT
-        	users.email AS email,
-        	(uptime * $1 + CAST(tasks_count as DOUBLE PRECISION) * $2) AS points,
-        	COUNT(users_ip.id) AS ips
+            users.email AS email,
+            (uptime * $1 + CAST(tasks_count as DOUBLE PRECISION) * $2) AS points
         FROM
-        	daily_stats
-      		JOIN users ON users.id = daily_stats.user_id
-      		JOIN users_ip ON users.id = users_ip.user_id
-        WHERE
-            day = $3
-            AND users_ip.updated_at >= NOW() - INTERVAL '24 hours'
-        GROUP BY
-	        users.email,
-	        points
-	    HAVING
-	        COUNT(users_ip.id) < 10
-        ORDER BY
-        	points DESC
-        	LIMIT $4
+	        daily_stats
+	        JOIN users ON users.id = daily_stats.user_id
+        WHERE day = $3
+        ORDER BY points DESC
+        LIMIT $4
         "#,
+        // r#"
+        // SELECT
+        // 	users.email AS email,
+        // 	(uptime * $1 + CAST(tasks_count as DOUBLE PRECISION) * $2) AS points,
+        // 	COUNT(users_ip.id) AS ips
+        // FROM
+        // 	daily_stats
+        // 	JOIN users ON users.id = daily_stats.user_id
+        // 	JOIN users_ip ON users.id = users_ip.user_id
+        // WHERE
+        //     day = $3
+        //     AND users_ip.updated_at >= NOW() - INTERVAL '24 hours'
+        // GROUP BY
+        //     users.email,
+        //     points
+        // HAVING
+        //     COUNT(users_ip.id) < 10
+        // ORDER BY
+        // 	points DESC
+        // 	LIMIT $4
+        // "#,
         uptime_factor,
         tasks_factor,
         day,
@@ -50,6 +50,5 @@ pub async fn get_daily_leaderboard(
     )
     .fetch_all(&mut **transaction)
     .await?;
-    query.insert(day, daily_stats.clone());
     Ok(daily_stats)
 }
--- libs/block-mesh-manager/src/routes/leaderboard/daily_leaderboard.rs
+++ libs/block-mesh-manager/src/routes/leaderboard/daily_leaderboard.rs
@@ -1,44 +1,58 @@
-#[allow(unused_imports)]
 use crate::database::leaderboard::get_daily_leaderboard::get_daily_leaderboard;
 use crate::errors::error::Error;
 use crate::middlewares::authentication::Backend;
-#[allow(unused_imports)]
 use crate::utils::points::{TASKS_FACTOR, UPTIME_FACTOR};
 use axum::{Extension, Json};
 use axum_login::AuthSession;
 use block_mesh_common::interfaces::server_api::{DailyLeaderboard, LeaderBoardUser};
-use chrono::{Duration, Utc};
+use chrono::{Duration, NaiveDate, Utc};
+use dashmap::DashMap;
 use sqlx::PgPool;
+use std::sync::Arc;
+use tokio::sync::OnceCell;
+
+type DailyLeaderBoardCache = Arc<DashMap<NaiveDate, Vec<LeaderBoardUser>>>;
+
+static CACHE: OnceCell<DailyLeaderBoardCache> = OnceCell::const_new();
 
 pub async fn handler(
     Extension(pool): Extension<PgPool>,
     Extension(auth): Extension<AuthSession<Backend>>,
 ) -> Result<Json<DailyLeaderboard>, Error> {
-    let transaction = pool.begin().await.map_err(Error::from)?;
     let day = Utc::now().date_naive() - Duration::days(1);
-    let _user = auth.user.ok_or(Error::UserNotFound)?;
-    let leaderboard_users: Vec<LeaderBoardUser> = vec![];
-    // let leaderboard_users: Vec<LeaderBoardUser> =
-    //     get_daily_leaderboard(&mut transaction, UPTIME_FACTOR, TASKS_FACTOR, 5)
-    //         .await?
-    //         .into_iter()
-    //         .map(|i| {
-    //             if user.email == i.email {
-    //                 LeaderBoardUser {
-    //                     email: user.email.clone(),
-    //                     points: i.points,
-    //                     ips: i.ips,
-    //                 }
-    //             } else {
-    //                 LeaderBoardUser {
-    //                     email: "***@***".to_string(),
-    //                     points: i.points,
-    //                     ips: i.ips,
-    //                 }
-    //             }
-    //         })
-    //         .collect();
+    let cache = CACHE
+        .get_or_init(|| async { Arc::new(DashMap::new()) })
+        .await;
+    if let Some(entry) = cache.get(&day) {
+        let leaderboard_users = entry.value().clone();
+        return Ok(Json(DailyLeaderboard {
+            leaderboard_users,
+            day,
+        }));
+    }
+    let mut transaction = pool.begin().await.map_err(Error::from)?;
+    let user = auth.user.ok_or(Error::UserNotFound)?;
+
+    let leaderboard_users: Vec<LeaderBoardUser> =
+        get_daily_leaderboard(&mut transaction, UPTIME_FACTOR, TASKS_FACTOR, 5)
+            .await?
+            .into_iter()
+            .map(|i| {
+                if user.email == i.email {
+                    LeaderBoardUser {
+                        email: user.email.clone(),
+                        points: i.points,
+                    }
+                } else {
+                    LeaderBoardUser {
+                        email: "***@***".to_string(),
+                        points: i.points,
+                    }
+                }
+            })
+            .collect();
     transaction.commit().await.map_err(Error::from)?;
+    cache.insert(day, leaderboard_users.clone());
     Ok(Json(DailyLeaderboard {
         leaderboard_users,
         day,
