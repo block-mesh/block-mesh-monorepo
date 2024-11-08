1d1acc2910122166fa4ca7c1cc496ca682c8d353
ohaddahan <ohaddahan@gmail.com>
2024-11-09 00:12:46 +0200

    Prep

--- libs/block-mesh-manager/src/routes/password/new_password_post.rs
+++ libs/block-mesh-manager/src/routes/password/new_password_post.rs
@@ -3,18 +3,26 @@ use crate::database::nonce::get_nonce_by_user_id::get_nonce_by_user_id;
 use crate::database::user::get_user_by_email::get_user_opt_by_email;
 use crate::database::user::update_user_password::update_user_password;
 use crate::errors::error::Error;
+use crate::middlewares::authentication::del_from_redis_with_pattern;
 use crate::notification::notification_redirect::NotificationRedirect;
+use crate::startup::application::AppState;
+use axum::extract::State;
 use axum::response::Redirect;
 use axum::{Extension, Form};
 use bcrypt::{hash, DEFAULT_COST};
+use block_mesh_common::interfaces::db_messages::InvalidateApiCache;
 use block_mesh_common::interfaces::server_api::NewPasswordForm;
 use block_mesh_common::routes_enum::RoutesEnum;
+use block_mesh_manager_database_domain::domain::notify_api::notify_api;
 use sqlx::PgPool;
+use std::sync::Arc;
 
 pub async fn handler(
+    State(state): State<Arc<AppState>>,
     Extension(pool): Extension<PgPool>,
     Form(form): Form<NewPasswordForm>,
 ) -> Result<Redirect, Error> {
+    let mut redis = state.redis.clone();
     let mut transaction = pool.begin().await.map_err(Error::from)?;
     let email = form.email.clone().to_ascii_lowercase();
     if form.password_confirm != form.password {
@@ -38,6 +46,16 @@ pub async fn handler(
     update_user_password(&mut transaction, user.id, hashed_password).await?;
     update_api_token(&mut transaction, user.id).await?;
     transaction.commit().await.map_err(Error::from)?;
+    del_from_redis_with_pattern(&email, "-*", &mut redis).await?;
+    del_from_redis_with_pattern(&user.id.to_string(), "-*", &mut redis).await?;
+    let _ = notify_api(
+        &state.pool,
+        InvalidateApiCache {
+            user_id,
+            email: user.email,
+        },
+    )
+    .await;
     Ok(NotificationRedirect::redirect(
         "Password updated",
         "Please use the new password and login",
