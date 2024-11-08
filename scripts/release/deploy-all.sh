fd2b55ba453bc9e49a20a227b8ded26faa5c1f67
ohaddahan <ohaddahan@gmail.com>
2024-11-09 00:59:49 +0200

    Deploy

--- scripts/release/deploy-all.sh
+++ scripts/release/deploy-all.sh
@@ -4,9 +4,9 @@ set -eo pipefail
 export _PWD="$(pwd)"
 export ROOT="$(git rev-parse --show-toplevel)"
 
-"${ROOT}/scripts/release/api-block-mesh-manager.sh"
-"${ROOT}/scripts/release/block-mesh-manager.sh"
 "${ROOT}/scripts/release/feature-flags-server.sh"
+"${ROOT}/scripts/release/api-block-mesh-manager.sh"
 "${ROOT}/scripts/release/worker-block-mesh-manager.sh"
 "${ROOT}/scripts/release/ws-block-mesh-manager.sh"
+"${ROOT}/scripts/release/block-mesh-manager.sh"
 "${ROOT}/scripts/release/tg-privacy-bot.sh"
