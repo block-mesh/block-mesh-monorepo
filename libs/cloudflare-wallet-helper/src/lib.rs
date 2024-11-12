65d1b72437ebbe953fe27b3b424689543eb8c0bb
ohaddahan <ohaddahan@gmail.com>
2024-11-12 15:04:07 +0200

    wallet helper

---
+++ libs/cloudflare-wallet-helper/src/lib.rs
@@ -0,0 +1,35 @@
+use askama::Template;
+use tracing_subscriber::fmt::format::Pretty;
+use tracing_subscriber::fmt::time::UtcTime;
+use tracing_subscriber::prelude::*;
+use tracing_web::{performance_layer, MakeConsoleWriter};
+use worker::*;
+
+use block_mesh_common::constants::{
+    BLOCK_MESH_GITBOOK, BLOCK_MESH_GITHUB, BLOCK_MESH_LOGO, BLOCK_MESH_SUPPORT_CHAT,
+    BLOCK_MESH_SUPPORT_EMAIL, BLOCK_MESH_TWITTER,
+};
+
+#[event(start)]
+fn start() {
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .json()
+        .with_ansi(false) // Only partially supported across JavaScript runtimes
+        .with_timer(UtcTime::rfc_3339()) // std::time is not available in browsers
+        .with_writer(MakeConsoleWriter); // write events to the console
+    let perf_layer = performance_layer().with_details_from_fields(Pretty::default());
+    tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(perf_layer)
+        .init();
+}
+
+#[derive(Template)]
+#[template(path = "home.html")]
+struct Home {}
+
+#[event(fetch)]
+async fn main(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+    let response = Home {}.render().unwrap();
+    Response::from_html(response)
+}
