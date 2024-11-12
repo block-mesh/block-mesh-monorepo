baee1fc3a4c2d59b73d354a3e9a448904c823ef8
ohaddahan <ohaddahan@gmail.com>
2024-11-12 15:18:01 +0200

    Wallet helper (#747)
    * wallet helper
    
    * wallet helper

---
+++ libs/cloudflare-wallet-helper/README.md
@@ -0,0 +1,1 @@
+# Cloudflare Wallet Helper
---
+++ libs/cloudflare-wallet-helper/src/lib.rs
@@ -0,0 +1,30 @@
+use askama::Template;
+use tracing_subscriber::fmt::format::Pretty;
+use tracing_subscriber::fmt::time::UtcTime;
+use tracing_subscriber::prelude::*;
+use tracing_web::{performance_layer, MakeConsoleWriter};
+use worker::*;
+
+#[event(start)]
+fn start() {
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .json()
+        .with_ansi(false) // Only partially supported across JavaScript runtimes
+        .with_timer(UtcTime::rfc_3339()) // std::time is not available in browsers
+        .with_writer(MakeConsoleWriter); // write events to the console
+    let perf_layer = performance_layer().with_details_from_fields(Pretty::default());
+    tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(perf_layer)
+        .init();
+}
+
+#[derive(Template)]
+#[template(path = "home.html")]
+struct Home {}
+
+#[event(fetch)]
+async fn main(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+    let response = Home {}.render().unwrap();
+    Response::from_html(response)
+}
