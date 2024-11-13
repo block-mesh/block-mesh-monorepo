51404b0940196d42ef976944e6765fbd35c57218
ohaddahan <ohaddahan@gmail.com>
2024-11-13 13:24:10 +0200

    return check_token to extension (#752)

--- libs/extension/src/utils/check_token.rs
+++ libs/extension/src/utils/check_token.rs
@@ -1,24 +1,28 @@
 use block_mesh_common::interfaces::server_api::CheckTokenRequest;
 use block_mesh_common::routes_enum::RoutesEnum;
+use leptos::logging::log;
 
 #[allow(dead_code)]
 pub async fn check_token(
     blockmesh_url: &str,
     credentials: &CheckTokenRequest,
 ) -> anyhow::Result<()> {
-    // let blockmesh_url = if blockmesh_url.contains("app") {
-    //     blockmesh_url.replace("app", "api")
-    // } else {
-    //     blockmesh_url.to_string()
-    // };
+    let blockmesh_url = if blockmesh_url.contains("app") {
+        blockmesh_url.replace("app", "api")
+    } else {
+        blockmesh_url.to_string()
+    };
     let url = format!("{}/api{}", blockmesh_url, RoutesEnum::Api_CheckToken);
     let client = reqwest::Client::new();
-    Ok(client
+    log!("check_token - url = {}", url,);
+    let r = client
         .post(&url)
         .header("Content-Type", "application/json")
         .json(&credentials)
         .send()
-        .await?
-        .json()
-        .await?)
+        .await?;
+    log!("check_token - response = {:#?}", r);
+    let j: serde_json::Value = r.json().await?;
+    log!("check_token - json = {:#?}", j);
+    Ok(())
 }
