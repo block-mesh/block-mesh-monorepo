cfb649506b4da1331a362d807d1be33b2609c043
ohaddahan <ohaddahan@gmail.com>
2024-11-22 10:41:55 +0200

    fml (#807)

--- libs/block-mesh-manager/migrations/20241122074033_remove-dup-index.sql
+++
@@ -1,8 +0,0 @@
-DROP INDEX IF EXISTS aggregates_default_name_idx;
-DROP INDEX IF EXISTS aggregates_download_name_idx;
-DROP INDEX IF EXISTS aggregates_founder_twitter_name_idx;
-DROP INDEX IF EXISTS aggregates_latency_name_idx;
-DROP INDEX IF EXISTS aggregates_tasks_name_idx;
-DROP INDEX IF EXISTS aggregates_twitter_name_idx;
-DROP INDEX IF EXISTS aggregates_upload_name_idx;
-DROP INDEX IF EXISTS aggregates_uptime_name_idx;
