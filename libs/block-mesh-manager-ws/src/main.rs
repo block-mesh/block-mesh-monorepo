6cf34fa421807e0cad97700f78a7c74583aeb70b
ohaddahan <ohaddahan@gmail.com>
2024-11-22 10:01:41 +0200

    bump && redis counter reset (#804)

--- libs/block-mesh-manager-ws/src/main.rs
+++ libs/block-mesh-manager-ws/src/main.rs
@@ -2,6 +2,7 @@
 use anyhow::Context;
 #[allow(unused_imports)]
 use block_mesh_common::constants::BLOCKMESH_SERVER_UUID_ENVAR;
+use block_mesh_common::constants::BLOCKMESH_WS_REDIS_COUNT_KEY;
 use block_mesh_common::env::load_dotenv::load_dotenv;
 use block_mesh_manager_ws::app::app;
 use block_mesh_manager_ws::state::AppState;
@@ -11,6 +12,7 @@ use block_mesh_manager_ws::websocket::ws_base_msg_loop::ws_base_msg_loop;
 use block_mesh_manager_ws::websocket::ws_bulk_loop::ws_bulk_loop;
 use block_mesh_manager_ws::websocket::ws_keep_alive::ws_keep_alive;
 use logger_general::tracing::setup_tracing_stdout_only_with_sentry;
+use redis::{AsyncCommands, RedisResult};
 use std::sync::Arc;
 #[allow(unused_imports)]
 use std::time::Duration;
@@ -62,6 +64,8 @@ async fn run() -> anyhow::Result<()> {
     let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
     tracing::info!("Listening on {}", listener.local_addr()?);
     let state = Arc::new(AppState::new().await);
+    let mut redis = state.redis.clone();
+    let _: RedisResult<()> = redis.set(BLOCKMESH_WS_REDIS_COUNT_KEY, 0).await;
     let broadcaster = state.websocket_manager.broadcaster.clone();
     let p = state.pool.clone();
     let b = broadcaster.clone();
