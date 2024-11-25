1963c50540d3ec376e6e3551697467a6bb536846
ohaddahan <ohaddahan@gmail.com>
2024-11-25 23:08:02 +0200

    Improve auth_status and logout

--- libs/block-mesh-manager/src/routes/logout/get.rs
+++ libs/block-mesh-manager/src/routes/logout/get.rs
@@ -1,17 +1,25 @@
 use crate::errors::error::Error;
 use crate::middlewares::authentication::Backend;
+use crate::startup::application::AppState;
+use axum::extract::State;
 use axum::response::Redirect;
 use axum::Extension;
 use axum_login::AuthSession;
 use block_mesh_common::routes_enum::RoutesEnum;
+use std::sync::Arc;
 
 #[tracing::instrument(name = "logout", skip_all)]
 pub async fn handler(
+    State(state): State<Arc<AppState>>,
     Extension(mut auth): Extension<AuthSession<Backend>>,
 ) -> Result<Redirect, Error> {
-    auth.logout()
+    let user = auth
+        .logout()
         .await
         .map_err(|e| Error::Auth(e.to_string()))?;
+    if let Some(session_user) = user {
+        state.wallet_addresses.remove(&session_user.email);
+    }
     Ok(Redirect::to(
         RoutesEnum::Static_UnAuth_Login.to_string().as_str(),
     ))
