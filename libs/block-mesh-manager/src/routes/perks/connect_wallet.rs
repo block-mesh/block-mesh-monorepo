633cdd6cbda393af3964bdd834914ce71c394643
ohaddahan <ohaddahan@gmail.com>
2024-12-02 09:29:00 +0200

    Fix wallet connect (#849)
    * More verbose connect wallet
    
    * More verbose connect wallet

--- libs/block-mesh-manager/src/routes/perks/connect_wallet.rs
+++ libs/block-mesh-manager/src/routes/perks/connect_wallet.rs
@@ -12,6 +12,7 @@ use block_mesh_common::interfaces::server_api::{ConnectWalletRequest, ConnectWal
 use block_mesh_manager_database_domain::domain::aggregate::AggregateName;
 use block_mesh_manager_database_domain::domain::get_or_create_aggregate_by_user_and_name::get_or_create_aggregate_by_user_and_name_str;
 use block_mesh_manager_database_domain::domain::update_aggregate::update_aggregate;
+use database_utils::utils::instrument_wrapper::{commit_txn, create_txn};
 use serde_json::Value;
 use solana_sdk::pubkey::Pubkey;
 use solana_sdk::signature::Signature;
@@ -27,8 +28,8 @@ pub async fn handler(
     Extension(auth): Extension<AuthSession<Backend>>,
     Json(body): Json<ConnectWalletRequest>,
 ) -> Result<Json<ConnectWalletResponse>, Error> {
-    let mut transaction = pool.begin().await?;
     let user = auth.user.ok_or(Error::UserNotFound)?;
+    let mut transaction = create_txn(&pool).await?;
     let db_user = get_user_opt_by_email(&mut transaction, &user.email)
         .await?
         .ok_or(Error::UserNotFound)?;
@@ -39,7 +40,7 @@ pub async fn handler(
     if signature.verify(pubkey.as_ref(), message) {
         match db_user.wallet_address {
             None => {
-                add_perk_to_user(
+                if add_perk_to_user(
                     &mut transaction,
                     user.id,
                     PerkName::Wallet,
@@ -47,32 +48,55 @@ pub async fn handler(
                     0.0,
                     serde_json::from_str("{}").unwrap(),
                 )
-                .await?;
-                update_user_wallet(&mut transaction, user.id, &body.pubkey).await?;
+                .await
+                .is_err()
+                {
+                    return Ok(Json(ConnectWalletResponse { status: 52 }));
+                }
+                if update_user_wallet(&mut transaction, user.id, &body.pubkey)
+                    .await
+                    .is_err()
+                {
+                    return Ok(Json(ConnectWalletResponse { status: 56 }));
+                }
                 state
                     .wallet_addresses
                     .insert(user.email.clone(), Some(body.pubkey));
             }
             Some(wallet_address) => {
                 let name = format!("{}_{}", AggregateName::WalletChange, Uuid::new_v4());
-                let agg = get_or_create_aggregate_by_user_and_name_str(
+                let agg = match get_or_create_aggregate_by_user_and_name_str(
                     &mut transaction,
                     &name,
                     &db_user.id,
                 )
-                .await?;
-                let _ = update_aggregate(&mut transaction, &agg.id, &Value::from(wallet_address))
-                    .await?;
-                update_user_wallet(&mut transaction, user.id, &body.pubkey).await?;
+                .await
+                {
+                    Ok(a) => a,
+                    Err(_) => {
+                        return Ok(Json(ConnectWalletResponse { status: 72 }));
+                    }
+                };
+                if update_aggregate(&mut transaction, &agg.id, &Value::from(wallet_address))
+                    .await
+                    .is_err()
+                {
+                    return Ok(Json(ConnectWalletResponse { status: 77 }));
+                }
+                if update_user_wallet(&mut transaction, user.id, &body.pubkey)
+                    .await
+                    .is_err()
+                {
+                    return Ok(Json(ConnectWalletResponse { status: 82 }));
+                }
                 state
                     .wallet_addresses
                     .insert(user.email.clone(), Some(body.pubkey));
             }
         }
     } else {
-        tracing::error!("Signature verification failed.");
-        return Err(Error::SignatureMismatch);
+        return Ok(Json(ConnectWalletResponse { status: 90 }));
     }
-    transaction.commit().await?;
+    commit_txn(transaction).await?;
     Ok(Json(ConnectWalletResponse { status: 200 }))
 }
