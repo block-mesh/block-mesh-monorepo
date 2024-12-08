6a24cd0cde9b5aa0e4cc703d210571f91dd6249c
ohaddahan <ohaddahan@gmail.com>
2024-12-08 11:07:55 +0200

    ordinal

--- libs/block-mesh-manager-worker/src/utils/rand.rs
+++ libs/block-mesh-manager-worker/src/utils/rand.rs
@@ -4,16 +4,13 @@ use rand::Rng;
 use std::env;
 
 pub fn rand_factor(limit: i32) -> i32 {
-    let r_limit = {
-        let day = Utc::now().date_naive();
-        let ordinal = day.ordinal() as i32;
-        let f = ordinal
-            % env::var("ORDINAL_FACTOR")
-                .unwrap_or("3".to_string())
-                .parse()
-                .unwrap_or(3i32);
-        let mut rng = rand::thread_rng();
-        rng.gen_range(1..(limit * (1 + f)))
-    };
-    r_limit
+    let day = Utc::now().date_naive();
+    let ordinal = day.ordinal() as i32;
+    let f = ordinal
+        % env::var("ORDINAL_FACTOR")
+            .unwrap_or("3".to_string())
+            .parse()
+            .unwrap_or(3i32);
+    let mut rng = rand::thread_rng();
+    rng.gen_range(1..(limit * (1 + f)))
 }
