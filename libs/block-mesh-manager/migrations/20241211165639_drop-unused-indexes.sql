70f93a2536cfd0275907a50156b9d70818a8be26
ohaddahan <ohaddahan@gmail.com>
2024-12-11 19:30:59 +0200

    remove indexes

---
+++ libs/block-mesh-manager/migrations/20241211165639_drop-unused-indexes.sql
@@ -0,0 +1,6 @@
+DROP INDEX new_daily_stats_status;
+CREATE INDEX daily_stats_finalized_status ON daily_stats_finalized (status);
+CREATE INDEX daily_stats_default_status ON daily_stats_default (status);
+DROP INDEX daily_stats_ref_bonus;
+DROP INDEX daily_stats_ref_bonus_applied;
+DROP INDEX new_aggregates_status;
