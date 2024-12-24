8907dad103a051d2e6f3a317623f8f7290ef27aa
ohaddahan <ohaddahan@gmail.com>
2024-12-24 14:42:21 +0200

    lint

--- programs/merkle-distributor/src/utils.rs
+++ programs/merkle-distributor/src/utils.rs
@@ -4,6 +4,6 @@ use anchor_spl::associated_token::get_associated_token_address;
 
 pub fn assert_ata(ata: &Pubkey, owner: &Pubkey, mint: &Pubkey) -> Result<()> {
     let real_ata = get_associated_token_address(owner, mint);
-    require_keys_eq!(ata.clone(), real_ata, ErrorCode::OwnerMismatch);
+    require_keys_eq!(*ata, real_ata, ErrorCode::OwnerMismatch);
     Ok(())
 }
