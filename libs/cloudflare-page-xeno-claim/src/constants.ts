9550b3d718663e8f3315a30e18dd660e4760807b
ohaddahan <ohaddahan@gmail.com>
2024-12-25 15:13:06 +0200

    xeno vnars

--- libs/cloudflare-page-xeno-claim/src/constants.ts
+++ libs/cloudflare-page-xeno-claim/src/constants.ts
@@ -1,10 +1,9 @@
 import { PublicKey } from '@solana/web3.js'
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 
-export const mint = new PublicKey('3XP1qCMCKsNmCp2G2inog3ztvFKPJRsAoZBjMtv1geGQ')
+export const mint = new PublicKey(import.meta.env.VITE_MINT)
 
 export const network = WalletAdapterNetwork.Mainnet
-export const rpc = 'https://devnet.helius-rpc.com/?api-key=cb443ba5-0587-4bf8-8274-9194e993f45e'
-// export const rpc =  'https://radial-neat-fire.solana-mainnet.quiknode.pro/9cd8966a7147622cfa74581af240632b89a6109a'
-// export const rpc = 'https://cosmological-newest-sunset.solana-devnet.quiknode.pro/acc3f83bfc7d8b7e192949dedb444ad122c86112'
+export const rpc = import.meta.env.VITE_RPC
 
+console.log('rpc', rpc, ' mint', mint.toBase58())
