9775b73fa85d96a7f1f4728e8d0d95a531af83eb
ohaddahan <ohaddahan@gmail.com>
2024-12-25 17:33:51 +0200

    yay

--- scripts/airdrop/create-markers.ts
+++ scripts/airdrop/create-markers.ts
@@ -1,4 +1,4 @@
-import { Connection, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js'
+import { ComputeBudgetProgram, Connection, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js'
 import {
   getOrCreateTokenAccountInstruction,
   processTransaction, sleep
@@ -32,27 +32,35 @@ function getConnection(network: Network): Connection {
   if (network === Network.DEVNET) {
     return new Connection('https://devnet.helius-rpc.com/?api-key=cb443ba5-0587-4bf8-8274-9194e993f45e')
   } else {
-    return new Connection('https://api.mainnet-beta.solana.com')
+    return new Connection('https://mainnet.helius-rpc.com/?api-key=0b25ef9e-0fd3-4fb5-a5fb-deec31b9017b')
   }
 }
 
 async function main() {
-  const admin = loadWalletKey('~/.config/solana/id.json')
-  const connection = getConnection(Network.DEVNET)
-  const mint = new PublicKey('3XP1qCMCKsNmCp2G2inog3ztvFKPJRsAoZBjMtv1geGQ')
+  const admin = loadWalletKey('/Users/ohaddahan/.config/solana/id.json')
+  const connection = getConnection(Network.MAINNET)
+  const mint = new PublicKey('Db7ZUaWTThwZy7bVhjn5Dda8D3fbbAhihcxPV4m9pump')
 
 
+  console.log('Here 1')
   const airDropped = await getAirDropperAccount(connection)
   console.log('airDropped', airDropped.pretty())
-  const claimMarker = await getClaimMarkerAccount(connection, admin.publicKey)
-  console.log('claimMarker', claimMarker.pretty())
-  // return
+  console.log('Here 2')
+  // const claimMarker = await getClaimMarkerAccount(connection, admin.publicKey)
+  // console.log('claimMarker', claimMarker.pretty())
 
-  const to = new PublicKey('Eic4L8eAagxmw4Pud8AmAeKQkhJC7vaPzo8GqBxx5dfo')
+  const to = new PublicKey('HQ6SbfdKsPuvSBrkq538tvZjASoqcdZUTfSYgLe7jSSH')
 
-  const instruction = createMarker(admin.publicKey, mint, to, LAMPORTS_PER_SOL)
+  const modifyComputeUnits = ComputeBudgetProgram.setComputeUnitLimit({
+    units: 100000
+  })
+  const addPriorityFee = ComputeBudgetProgram.setComputeUnitPrice({
+    microLamports: 10000
+  })
+
+  const instruction = createMarker(admin.publicKey, mint, to, LAMPORTS_PER_SOL / 1000)
   const sigx = await processTransaction(
-    [instruction],
+    [modifyComputeUnits, addPriorityFee, instruction],
     connection,
     admin
   )
