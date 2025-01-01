614ebb4d24685336d6b29fa3b6e3fabe5869ae79
ohaddahan <ohaddahan@gmail.com>
2025-01-01 10:33:33 +0200

    faster finalize

--- libs/block-mesh-manager-worker/src/cron_jobs/finalize_daily_cron.rs
+++ libs/block-mesh-manager-worker/src/cron_jobs/finalize_daily_cron.rs
@@ -1,15 +1,20 @@
 use crate::db_calls::bulk_finalize::bulk_finalize;
 use database_utils::utils::instrument_wrapper::{commit_txn, create_txn};
 use sqlx::PgPool;
+use std::env;
 use std::time::Duration;
 
 #[tracing::instrument(name = "finalize_daily_cron", skip_all)]
 pub async fn finalize_daily_cron(pool: PgPool) -> Result<(), anyhow::Error> {
+    let finalize_sleep = env::var("FINALIZE_SLEEP_SECS")
+        .unwrap_or("180".to_string())
+        .parse()
+        .unwrap_or(180);
     loop {
         if let Ok(mut transaction) = create_txn(&pool).await {
             let _ = bulk_finalize(&mut transaction).await;
             let _ = commit_txn(transaction).await;
         }
-        tokio::time::sleep(Duration::from_secs(60 * 60)).await;
+        tokio::time::sleep(Duration::from_secs(finalize_sleep)).await;
     }
 }
--- libs/block-mesh-manager-worker/src/db_calls/bulk_finalize.rs
+++ libs/block-mesh-manager-worker/src/db_calls/bulk_finalize.rs
@@ -1,10 +1,12 @@
-use chrono::{Duration, Utc};
 use sqlx::{Postgres, Transaction};
+use std::env;
 
 #[tracing::instrument(name = "bulk_finalize", skip_all, err)]
 pub async fn bulk_finalize(transaction: &mut Transaction<'_, Postgres>) -> anyhow::Result<()> {
-    let now = Utc::now() - Duration::days(1);
-    let day = now.date_naive();
+    let limit = env::var("FINALIZE_LIMIT")
+        .unwrap_or("100000".to_string())
+        .parse()
+        .unwrap_or(100_000);
     sqlx::query!(
         r#"
         UPDATE
@@ -14,13 +16,16 @@ pub async fn bulk_finalize(transaction: &mut Transaction<'_, Postgres>) -> anyho
             SELECT
             id
             FROM daily_stats
-            WHERE day < $2 AND status = $3
-            LIMIT 100000
+            WHERE
+                day < CURRENT_DATE
+            AND
+                status = $2
+            LIMIT $3
         )
         "#,
         "Finalized".to_string(),
-        day,
-        "OnGoing".to_string()
+        "OnGoing".to_string(),
+        limit
     )
     .execute(&mut **transaction)
     .await?;
