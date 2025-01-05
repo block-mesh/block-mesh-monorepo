e06bb1852c25eac5ceb88729694f262cc010fb10
ohaddahan <ohaddahan@gmail.com>
2025-01-05 13:48:29 +0200

    lint

--- libs/clickhouse-processing/src/main.rs
+++ libs/clickhouse-processing/src/main.rs
@@ -41,37 +41,35 @@ async fn main() -> anyhow::Result<()> {
         let lines = read_ljson_for_merge(&args.input_file)?;
         let vec: Vec<Output> = lines.values().cloned().collect();
         write_to_file_ljson(vec, "output.json");
+    } else if args.from_s3 {
+        let output_file = format!("{}/DONE__{}", args.output_dir, args.key);
+        let input_file = format!("{}/{}", args.input_dir, args.key);
+        let local_key = args.key.replace("/", "_");
+        download_file_from_s3(&args.bucket, &args.key, &local_key, &args.input_dir).await?;
+        let date = file_date(&input_file)?.to_string();
+        let raws = read_lson(&input_file)?;
+        let output = process_raw(raws, args.limit, date);
+        write_to_file_ljson(output, &output_file);
     } else {
-        if args.from_s3 {
-            let output_file = format!("{}/DONE__{}", args.output_dir, args.key);
-            let input_file = format!("{}/{}", args.input_dir, args.key);
-            let local_key = args.key.replace("/", "_");
-            download_file_from_s3(&args.bucket, &args.key, &local_key, &args.input_dir).await?;
-            let date = file_date(&input_file)?.to_string();
-            let raws = read_lson(&input_file)?;
-            let output = process_raw(raws, args.limit, date);
-            write_to_file_ljson(output, &output_file);
-        } else {
-            let files = read_files_from_dir(&args.input_dir)?;
-            files.par_iter().for_each(|file: &PathBuf| {
-                println!("file = {:?}", file);
-                let input_file = file.to_str().unwrap_or_default();
-                let key = file
-                    .file_name()
-                    .unwrap_or_default()
-                    .to_str()
-                    .unwrap_or_default();
-                let output_file = format!("{}/DONE__{}", args.output_dir, key);
-                if is_exists(&output_file) {
-                    println!("file = '{}' , already exists", output_file);
-                } else {
-                    let date = file_date(input_file).unwrap().to_string();
-                    let raws = read_lson(input_file).unwrap();
-                    let output = process_raw(raws, args.limit, date);
-                    write_to_file_ljson(output, &output_file);
-                }
-            });
-        }
+        let files = read_files_from_dir(&args.input_dir)?;
+        files.par_iter().for_each(|file: &PathBuf| {
+            println!("file = {:?}", file);
+            let input_file = file.to_str().unwrap_or_default();
+            let key = file
+                .file_name()
+                .unwrap_or_default()
+                .to_str()
+                .unwrap_or_default();
+            let output_file = format!("{}/DONE__{}", args.output_dir, key);
+            if is_exists(&output_file) {
+                println!("file = '{}' , already exists", output_file);
+            } else {
+                let date = file_date(input_file).unwrap().to_string();
+                let raws = read_lson(input_file).unwrap();
+                let output = process_raw(raws, args.limit, date);
+                write_to_file_ljson(output, &output_file);
+            }
+        });
     }
     Ok(())
 }
