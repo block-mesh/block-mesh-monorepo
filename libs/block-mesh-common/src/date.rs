adae10f3101e5957ebf19684db4ada09d46ba251
ohaddahan <ohaddahan@gmail.com>
2025-01-12 13:38:04 +0200

    lint

--- libs/block-mesh-common/src/date.rs
+++ libs/block-mesh-common/src/date.rs
@@ -3,8 +3,8 @@ use std::cmp::{max, min};
 
 pub fn date_range(i_since: &NaiveDate, i_until: &NaiveDate) -> Vec<(NaiveDate, NaiveDate)> {
     let mut dates = Vec::new();
-    let since = min(i_since, i_until).clone();
-    let until = max(i_since, i_until).clone();
+    let since = *min(i_since, i_until);
+    let until = *max(i_since, i_until);
     let mut current_date = since;
     while current_date <= until {
         let pair = (current_date, current_date + Duration::days(1));
