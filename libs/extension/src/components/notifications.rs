cc47e3e821d9c2707e7f2feedb7f73fc171b6f23
ohaddahan <ohaddahan@gmail.com>
2025-02-02 12:17:19 +0200

    fix z-index

--- libs/extension/src/components/notifications.rs
+++ libs/extension/src/components/notifications.rs
@@ -18,6 +18,7 @@ pub fn ExtensionWrapperNotifications() -> impl IntoView {
     view! {
         <div
             aria-live="assertive"
+            style="z-index: 100"
             class="pointer-events-none fixed inset-0 flex items-start px-4 py-6 sm:items-start sm:p-6"
         >
             <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
