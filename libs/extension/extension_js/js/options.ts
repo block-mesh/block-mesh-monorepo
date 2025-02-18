6c7be5bee250ccc89ba2dbcb245a7ea7ffd627c5
ohaddahan <ohaddahan@gmail.com>
2025-02-18 13:22:06 +0200

    typescript lints

--- libs/extension/extension_js/js/options.ts
+++ libs/extension/extension_js/js/options.ts
@@ -1,35 +1,36 @@
-import initWasmModule, {mount_options} from './wasm/blockmesh_ext.js';
+// @ts-ignore
+import initWasmModule, { mount_options } from './wasm/blockmesh_ext.js'
 
-function onSuccess(message) {
-    try {
-        console.log(`onSuccess: ${JSON.stringify(message)}`);
-    } catch (e) {
-        console.error(`onSuccess error: ${e}`);
-    }
+function onSuccess(message: any) {
+  try {
+    console.log(`onSuccess: ${JSON.stringify(message)}`)
+  } catch (e) {
+    console.error(`onSuccess error: ${e}`)
+  }
 }
 
-function onError(error) {
-    try {
-        console.error(`onError: ${JSON.stringify(error)}`);
-    } catch (e) {
-        console.error(`onError error (1): ${error}`);
-        console.error(`onError error (2): ${e}`);
-    }
+function onError(error: any) {
+  try {
+    console.error(`onError: ${JSON.stringify(error)}`)
+  } catch (e) {
+    console.error(`onError error (1): ${error}`)
+    console.error(`onError error (2): ${e}`)
+  }
 }
 
-document.addEventListener('DOMContentLoaded', async function () {
-    await initWasmModule().then(onSuccess, onError);
-    mount_options();
-    let delete_form = document.getElementById("delete_form");
-    if (delete_form) {
-        delete_form.parentNode.removeChild(delete_form);
-    }
-});
+document.addEventListener('DOMContentLoaded', async function() {
+  await initWasmModule().then(onSuccess, onError)
+  mount_options()
+  let delete_form = document.getElementById('delete_form')
+  if (delete_form) {
+    delete_form?.parentNode?.removeChild(delete_form)
+  }
+})
 
 chrome.runtime.onMessage.addListener((msg, sender) => {
-        if (msg !== "ping") {
-            console.log("Options::onMessage", {msg, sender});
-        }
-        return true
+    if (msg !== 'ping') {
+      console.log('Options::onMessage', { msg, sender })
     }
-);
+    return true
+  }
+)
