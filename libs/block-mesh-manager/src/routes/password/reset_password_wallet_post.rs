08afbed00557d36560f87aa663c5e0af5fbb6421
ohaddahan <ohaddahan@gmail.com>
2025-02-24 17:51:36 +0200

    cleanup

--- libs/block-mesh-manager/src/routes/password/reset_password_wallet_post.rs
+++ libs/block-mesh-manager/src/routes/password/reset_password_wallet_post.rs
@@ -3,11 +3,8 @@ use std::sync::Arc;
 
 use crate::database::api_token::update_api_token::update_api_token;
 use crate::database::nonce::get_nonce_by_user_id::get_nonce_by_user_id;
-use crate::database::perks::add_perk_to_user::add_perk_to_user;
 use crate::database::user::get_user_by_wallet::get_user_opt_by_wallet;
 use crate::database::user::update_user_password::update_user_password;
-use crate::database::user::update_user_wallet::update_user_wallet;
-use crate::domain::perk::PerkName;
 use crate::errors::error::Error;
 use crate::middlewares::authentication::{Backend, Credentials};
 use crate::startup::application::AppState;
@@ -61,33 +58,19 @@ pub async fn handler(
     let signature =
         Signature::try_from(sig_array.0.as_slice()).map_err(|_| Error::InternalServer)?;
     let pubkey = Pubkey::from_str(form.pubkey.as_str()).unwrap_or_default();
-    let form_nonce = form.nonce.clone();
     let message = form.nonce.as_bytes();
-    let mem_nonce = state.wallet_login_nonce.get(&form_nonce).await;
-    match mem_nonce {
-        Some(mem_nonce) => {
-            if mem_nonce != form_nonce {
-                return Ok(Error::redirect(
-                    400,
-                    "Retry please",
-                    "Invalid nonce",
-                    RoutesEnum::Static_UnAuth_Register_Wallet
-                        .to_string()
-                        .as_str(),
-                ));
-            }
-        }
-        None => {
-            return Ok(Error::redirect(
-                400,
-                "Retry please",
-                "Missing nonce",
-                RoutesEnum::Static_UnAuth_Register_Wallet
-                    .to_string()
-                    .as_str(),
-            ));
-        }
+    let user = get_user_opt_by_wallet(&mut transaction, &form.pubkey).await?;
+    if user.is_none() {
+        return Ok(Error::redirect(
+            400,
+            "User Doesnt Exists",
+            "User with this wallet doesnt",
+            RoutesEnum::Static_UnAuth_Register_Wallet
+                .to_string()
+                .as_str(),
+        ));
     }
+    let user = user.unwrap();
     if form.password.contains(' ') {
         return Ok(Error::redirect(
             400,
@@ -116,7 +99,6 @@ pub async fn handler(
                 .as_str(),
         ));
     }
-
     if form.password_confirm != form.password {
         return Ok(Error::redirect(
             400,
@@ -127,20 +109,6 @@ pub async fn handler(
                 .as_str(),
         ));
     }
-
-    let user = get_user_opt_by_wallet(&mut transaction, &form.pubkey).await?;
-    if user.is_none() {
-        return Ok(Error::redirect(
-            400,
-            "User Doesnt Exists",
-            "User with this wallet doesnt",
-            RoutesEnum::Static_UnAuth_Register_Wallet
-                .to_string()
-                .as_str(),
-        ));
-    }
-
-    let user = user.unwrap();
     let user_id = user.id;
     let hashed_password = hash(form.password.clone(), DEFAULT_COST)?;
     let email = user.email;
@@ -158,18 +126,7 @@ pub async fn handler(
     let nonce = nonce.unwrap().nonce.expose_secret().to_string();
     update_user_password(&mut transaction, user.id, hashed_password).await?;
     update_api_token(&mut transaction, user.id).await?;
-
     if signature.verify(pubkey.as_ref(), message) {
-        add_perk_to_user(
-            &mut transaction,
-            user_id,
-            PerkName::Wallet,
-            1.1,
-            0.0,
-            serde_json::from_str("{}").unwrap(),
-        )
-        .await?;
-        update_user_wallet(&mut transaction, user_id, &form.pubkey).await?;
         state
             .wallet_addresses
             .insert(email.clone(), Some(form.pubkey), None)
