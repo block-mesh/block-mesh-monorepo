48b27a7f2415e63fe01e663763e074e8a19845a3
ohaddahan <ohaddahan@gmail.com>
2025-03-17 11:27:02 +0200

    sql work

--- libs/block-mesh-manager-worker/src/db_calls/bulk_task_bonus.rs
+++ libs/block-mesh-manager-worker/src/db_calls/bulk_task_bonus.rs
@@ -2,6 +2,7 @@ use crate::utils::rand::rand_factor;
 use anyhow::anyhow;
 use sqlx::postgres::PgQueryResult;
 use sqlx::{Postgres, Transaction};
+use std::env;
 
 #[tracing::instrument(name = "bulk_task_bonus", skip(transaction), ret, err, level = "trace")]
 pub async fn bulk_task_bonus(
@@ -13,24 +14,37 @@ pub async fn bulk_task_bonus(
         return Err(anyhow!("bulk_task_bonus called without a limit and bonus"));
     }
     let r_limit = rand_factor(limit);
+    let bulk_task_bonus_query_limit = env::var("BULK_TASK_BONUS_QUERY_LIMIT")
+        .unwrap_or("1000".to_string())
+        .parse()
+        .unwrap_or(1_000);
+
     let r = sqlx::query!(
         r#"
+        WITH limited_rows AS (
+            SELECT ds.id
+            FROM daily_stats ds
+            JOIN aggregates a ON ds.user_id = a.user_id
+            WHERE a.name = 'Uptime'
+                AND a.updated_at >= NOW() - INTERVAL '2 hour'
+                AND ds.status = 'OnGoing'
+                AND ds.day = CURRENT_DATE
+                AND ds.tasks_count < $2
+            LIMIT $3
+        )
         UPDATE daily_stats ds
-            SET
-                tasks_count = GREATEST(tasks_count, LEAST(tasks_count + $1, $2)),
-                tasks_count_bonus = GREATEST(tasks_count_bonus, tasks_count_bonus + (LEAST(tasks_count + $1, $2) - tasks_count)),
-                updated_at = now()
-        FROM aggregates a
-        WHERE
-            ds.user_id = a.user_id
-            AND a.name = 'Uptime'
-            AND a.updated_at >= NOW() - INTERVAL '2 hour'
-            AND ds.status = 'OnGoing'
-            AND ds.day = CURRENT_DATE
-            AND ds.tasks_count < $2
+        SET
+            tasks_count = GREATEST(tasks_count, LEAST(tasks_count + $1, $2)),
+            tasks_count_bonus = GREATEST(tasks_count_bonus, tasks_count_bonus + (LEAST(tasks_count + $1, $2) - tasks_count)),
+            updated_at = NOW()
+        FROM limited_rows lr
+        WHERE ds.id = lr.id
+        AND ds.status = 'OnGoing'
+        AND ds.day = CURRENT_DATE
         "#,
         bonus,
-        limit + r_limit
+        limit + r_limit,
+        bulk_task_bonus_query_limit
     )
     .execute(&mut **transaction)
     .await?;
--- libs/block-mesh-manager-worker/src/db_calls/bulk_uptime_bonus.rs
+++ libs/block-mesh-manager-worker/src/db_calls/bulk_uptime_bonus.rs
@@ -2,6 +2,7 @@ use crate::utils::rand::rand_factor;
 use anyhow::anyhow;
 use sqlx::postgres::PgQueryResult;
 use sqlx::{Postgres, Transaction};
+use std::env;
 
 #[tracing::instrument(
     name = "bulk_uptime_bonus",
@@ -17,40 +18,37 @@ pub async fn bulk_uptime_bonus(
     if bonus.is_nan() || bonus <= 0.0 || bonus.is_infinite() {
         return Err(anyhow!("bulk uptime bonus must be a positive integer"));
     }
+    let bulk_uptime_bonus_query_limit = env::var("BULK_UPTIME_BONUS_QUERY_LIMIT")
+        .unwrap_or("1000".to_string())
+        .parse()
+        .unwrap_or(1_000);
     let r_limit = rand_factor(86400);
     let r = sqlx::query!(
-        // r#"
-        // WITH updates (id, value) AS (
-        // 	SELECT id,value FROM aggregates
-        // 	WHERE name = 'Uptime'
-        // 	AND value != 'null'
-        // 	AND updated_at < now() - interval '5 minutes'
-        //     FOR UPDATE SKIP LOCKED
-        // )
-        // UPDATE aggregates
-        // SET
-        //     value = to_jsonb((COALESCE(NULLIF(aggregates.value, 'null'), '0')::text)::double precision + $1),
-        //     updated_at = now()
-        // FROM updates
-        // WHERE aggregates.id = updates.id
-        // "#,
         r#"
-            UPDATE daily_stats ds
-                SET
-                    uptime = GREATEST(uptime, LEAST(uptime + $1, $2)),
-                    uptime_bonus = GREATEST(uptime_bonus, LEAST(uptime_bonus + $1, $2)),
-                    updated_at = now()
-            FROM aggregates a
-            WHERE
-                ds.user_id = a.user_id
-                AND a.name = 'Uptime'
+        WITH limited_rows AS (
+            SELECT ds.id
+            FROM daily_stats ds
+            JOIN aggregates a ON ds.user_id = a.user_id
+            WHERE a.name = 'Uptime'
                 AND a.updated_at >= NOW() - INTERVAL '2 hour'
-            	AND ds.status = 'OnGoing'
+                AND ds.status = 'OnGoing'
                 AND ds.day = CURRENT_DATE
                 AND ds.uptime < $2
+            LIMIT $3
+        )
+        UPDATE daily_stats ds
+        SET
+            uptime = GREATEST(uptime, LEAST(uptime + $1, $2)),
+            uptime_bonus = GREATEST(uptime_bonus, LEAST(uptime_bonus + $1, $2)),
+            updated_at = NOW()
+        FROM limited_rows lr
+        WHERE ds.id = lr.id
+            AND ds.status = 'OnGoing'
+            AND ds.day = CURRENT_DATE
         "#,
         bonus,
-        86400.0 + r_limit as f64
+        86400.0 + r_limit as f64,
+        bulk_uptime_bonus_query_limit
     )
     .execute(&mut **transaction)
     .await?;
