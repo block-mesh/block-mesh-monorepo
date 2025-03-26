79f9dfb65c135519a0ec455655e421890f3d7e3f
ohaddahan <ohaddahan@gmail.com>
2025-03-26 16:05:41 +0200

    yay

--- scripts/release/ids.sh
+++ scripts/release/ids.sh
@@ -4,8 +4,8 @@ set -eo pipefail
 
 heroku container:login
 docker pull --platform linux/amd64 blockmesh/ids:latest
-docker tag blockmesh/ids:latest registry.heroku.com/ids/web
-docker push registry.heroku.com/ids/web
+docker tag blockmesh/ids:latest registry.heroku.com/ids-blockmesh/web
+docker push registry.heroku.com/ids-blockmesh/web
 #heroku container:push web -a feature-flags-server
-heroku container:release web -a ids
-heroku restart -a ids
+heroku container:release web -a ids-blockmesh
+heroku restart -a ids-blockmesh
