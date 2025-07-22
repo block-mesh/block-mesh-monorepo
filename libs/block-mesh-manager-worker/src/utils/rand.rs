7b732a8681c9e1bdbe0b83d9005e4e738106c5dc
ohaddahan <ohaddahan@gmail.com>
2025-07-22 11:31:51 +0300

    bump fix

--- libs/block-mesh-manager-worker/src/utils/rand.rs
+++ libs/block-mesh-manager-worker/src/utils/rand.rs
@@ -1,16 +1,7 @@
-use chrono::Datelike;
-use chrono::Utc;
 use rand::Rng;
-use std::env;
+use std::cmp::max;
 
 pub fn rand_factor(limit: i32) -> i32 {
-    let day = Utc::now().date_naive();
-    let ordinal = day.ordinal() as i32;
-    let f = ordinal
-        % env::var("ORDINAL_FACTOR")
-            .unwrap_or("3".to_string())
-            .parse()
-            .unwrap_or(3i32);
     let mut rng = rand::thread_rng();
-    rng.gen_range(1..(limit * (1 + f)))
+    rng.gen_range(1..max(limit, 2))
 }
