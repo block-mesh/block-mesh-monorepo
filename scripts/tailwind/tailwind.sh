cbc0b6e52941bc00e368a60a5bd5144c780134db
ohaddahan <ohaddahan@gmail.com>
2025-08-18 14:55:33 +0300

    Tailwind , add --remote to script

--- scripts/tailwind/tailwind.sh
+++ scripts/tailwind/tailwind.sh
@@ -7,5 +7,5 @@ cd "${ROOT}" || exit 1
 
 ensure npx tailwindcss -i ./input.css -o ./tailwind.css
 ensure npx tailwindcss -i ./extension-input.css -o ./extension.css
-ensure wrangler r2 object put assets/tailwind.css --file tailwind.css
-ensure wrangler r2 object put assets/extension.css --file extension.css
+ensure npx wrangler r2 object put assets/tailwind.css --file tailwind.css --remote
+ensure npx wrangler r2 object put assets/extension.css --file extension.css --remote
