9b7cae6370b0aa1c88fe1e0414e3d0813a8239f7
ohaddahan <ohaddahan@gmail.com>
2026-03-23 14:15:17 +0200

    hate it (#1153)

--- libs/block-mesh-manager/migrations/20260323120000_add_wallet_connected_sent_and_backfill_snag_flags.sql
+++ libs/block-mesh-manager/migrations/20260323120000_add_wallet_connected_sent_and_backfill_snag_flags.sql
@@ -2,9 +2,7 @@ ALTER TABLE users
     ADD COLUMN wallet_connected_sent BOOLEAN NOT NULL DEFAULT FALSE;
 
 UPDATE users
-SET extension_activated_sent = FALSE;
-
-UPDATE users
-SET wallet_connected_sent = FALSE;
+SET extension_activated_sent = FALSE
+WHERE extension_activated_sent = TRUE;
 
 CREATE INDEX IF NOT EXISTS users_wallet_connected_sent ON users (wallet_connected_sent);
